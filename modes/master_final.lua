1cbd9e717ae9da42d796968ff8da99ae9798845a
MrZ_26 <MrZ626@users.github.com>
2020-08-03 04:01:24 +0800

    stage显示数字错误

--- modes/master_final.lua
+++ modes/master_final.lua
@@ -15,19 +15,19 @@ local function score(P)
 		local s=MD.event+1;MD.event=s
 		local E=P.gameEnv
 		if s<4 then
+			P:showTextF(text.stage(s),0,-120,80,"fly")
 			--First 300
 			if s~=1 then E.lock=E.lock-1 end
 			if s~=2 then E.wait=E.wait-1 end
 			if s~=3 then E.fall=E.fall-1 end
-			P:showTextF(text.stage(s),0,-120,80,"fly")
 		elseif s<10 then
+			P:showTextF(text.stage(s),0,-120,60,"fly",1.26)
 			if s==4 or s==7 then E.das=E.das-1 end
 			s=s%3
 			if s==0 then E.lock=E.lock-1
 			elseif s==1 then E.wait=E.wait-1
 			elseif s==2 then E.fall=E.fall-1
 			end
-			P:showTextF(text.stage(s),0,-120,80,"fly")
 		else
 			MD.point,MD.event=1000,9
 			P:win("finish")
