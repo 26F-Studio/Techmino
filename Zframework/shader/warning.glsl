e746cf7bb1a793ae0761b119177909c67859e5a2
MrZ_26 <MrZ626@users.github.com>
2020-08-09 14:32:30 +0800

    死亡预警在两个方向的宽度调整

--- Zframework/shader/warning.glsl
+++ Zframework/shader/warning.glsl
@@ -3,6 +3,6 @@ extern float level;
 vec4 effect(vec4 color,Image text,vec2 pos,vec2 scr_pos){
 	float dx=abs(scr_pos.x/w-0.5);
 	float dy=abs(scr_pos.y/h-0.5);
-	float a=(max(dx,dy)*2.-.626)*level;
+	float a=(max(dx*2.6,dy*1.8)-.626)*level;
 	return vec4(1.,0.,0.,a);
 }
