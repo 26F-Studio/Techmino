4f733772d33ca13110c218e438adc47111b33ed8
MrZ626 <1046101471@qq.com>
2020-10-26 00:38:13 +0800

    新功能：音效室（初版）

--- parts/scenes.lua
+++ parts/scenes.lua
@@ -2011,6 +2011,7 @@ do--custom_sequence
 				while BAG[p]==key do p=p+1 end
 				ins(BAG,p,key)
 				S.cur=p
+				SFX.play("lock")
 			end
 		end
 	end
@@ -2363,6 +2364,7 @@ do--custom_mission
 			if missionEnum[input]then
 				S.cur=S.cur+1
 				ins(MISSION,S.cur,missionEnum[input])
+				SFX.play("lock")
 				input=""
 			elseif #input>1 or not legalInput[input]then
 				input=""
@@ -2901,6 +2903,48 @@ do--login
 	end
 end
 do--account
+end
+do--sound
+	function sceneInit.sound()
+		sceneTemp={
+			mini=false,
+			b2b=false,
+			b3b=false,
+			pc=false,
+		}
+	end
+
+	local blockName={"z","s","j","l","t","o","i"}
+	local lineCount={
+		"single",
+		"double",
+		"triple",
+	}
+	function keyDown.sound(key)
+		local S=sceneTemp
+		if key=="1"then
+			S.mini=not S.mini
+		elseif key=="2"then
+			S.b2b=not S.b2b
+			if S.b2b then S.b3b=false end
+		elseif key=="3"then
+			S.b3b=not S.b3b
+			if S.b3b then S.b2b=false end
+		elseif key=="4"then
+			S.pc=not S.pc
+		elseif type(key)=="number"then
+			local CHN=VOC.getFreeChannel()
+			if S.mini then VOC.play("mini",CHN)end
+			if S.b2b then VOC.play("b2b",CHN)
+			elseif S.b3b then VOC.play("b3b",CHN)
+			end
+			VOC.play(blockName[int(key/10)].."spin",CHN)
+			if key%10>0 then VOC.play(lineCount[key%10],CHN)end
+			if S.pc then VOC.play("perfect_clear",CHN)end
+		elseif key=="escape"then
+			SCN.back()
+		end
+	end
 end
 do--minigame
 	function sceneInit.minigame()
