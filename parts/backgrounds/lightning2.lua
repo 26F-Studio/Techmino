791701209360c354ec97727a2d72cc484efbf44b
MrZ626 <1046101471@qq.com>
2020-11-08 19:36:49 +0800

    修复lightning2背景

--- parts/backgrounds/lightning2.lua
+++ parts/backgrounds/lightning2.lua
@@ -5,10 +5,12 @@ local back={}
 
 local t
 local colorLib
-local blocks=BLOCKS
-local scs=spinCenters
+local blocks
+local scs
 function back.init()
 	colorLib=SKIN.libColor
+	blocks=BLOCKS
+	scs=spinCenters
 	t=rnd()*2600
 end
 function back.update(dt)
@@ -22,6 +24,6 @@ function back.draw()
 	end
 	local _=colorLib[SETTING.skin[R]]
 	gc.setColor(_[1],_[2],_[3],.12)
-	gc.draw(TEXTURE.miniBlock[R],640,360,t%3.1416*6,400,400,scs[R][0][2]+.5,#blocks[R][0]-scs[R][0][1]-.5)
+	gc.draw(TEXTURE.miniBlock[R],SCR.w/2,SCR.h/2,t%3.1416*6,400*SCR.k,nil,scs[R][0][2]+.5,#blocks[R][0]-scs[R][0][1]-.5)
 end
 return back
