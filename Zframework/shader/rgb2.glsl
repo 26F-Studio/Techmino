e0846f6de3c40c571f2464cfa95aebd6e10d1c1a
MrZ626 <1046101471@qq.com>
2020-11-13 10:55:51 +0800

    微调rainbow2背景

--- Zframework/shader/rgb2.glsl
+++ Zframework/shader/rgb2.glsl
@@ -3,9 +3,9 @@ vec4 effect(vec4 color,Image text,vec2 pos,vec2 scr_pos){
 	float x=scr_pos.x/w;
 	float y=scr_pos.y/h;
 	return vec4(
-		.4-y*.3-.1*sin(t/6.26),
-		.2+.1*sin(t),
-		abs(.7-x*1.2+y*.5*sin(t/16.)),
+		.8-y*.8-.1*sin(t/6.26),
+		.4+.1*sin(t/4.)*(y+2.)/(y+5.),
+		abs(.7-x*1.4+y*.5*sin(t/16.)),
 		1.
 	);
 }
