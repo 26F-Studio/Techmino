5604ba54167b92e41fee2789f192ebf27134d918
MrZ626 <1046101471@qq.com>
2020-12-07 17:16:04 +0800

    0.12.4: 网战预备

--- conf.lua
+++ conf.lua
@@ -1,5 +1,5 @@
-VERSION_CODE=1203
-VERSION_NAME="Alpha V0.12.3"
+VERSION_CODE=1204
+VERSION_NAME="Alpha V0.12.4"
 love.setDeprecationOutput(false)
 function love.conf(t)
 	t.identity="Techmino"--Saving folder
