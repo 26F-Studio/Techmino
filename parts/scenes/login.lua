88a5e50256bafb78abdd720a588ac0a7f44db71e
MrZ626 <1046101471@qq.com>
2020-12-07 19:27:34 +0800

    由于不使用游戏内注册，所以暂时关闭注册入口

--- parts/scenes/login.lua
+++ parts/scenes/login.lua
@@ -64,7 +64,7 @@ local scene={}
 
 scene.widgetList={
 	WIDGET.newText{name="title",		x=80,	y=50,font=70,align="L"},
-	WIDGET.newButton{name="register",	x=1140,	y=100,w=170,h=80,color="green",code=function()SCN.swapTo("register","swipeR")end},
+	-- WIDGET.newButton{name="register",	x=1140,	y=100,w=170,h=80,color="green",code=function()SCN.swapTo("register","swipeR")end},
 	WIDGET.newTextBox{name="email",		x=380,	y=200,w=500,h=60,regex="[0-9A-Za-z@._-]"},
 	WIDGET.newTextBox{name="password",	x=380,	y=300,w=626,h=60,secret=true,regex="[ -~]"},
 	WIDGET.newKey{name="login",			x=1140,	y=540,w=170,h=80,font=40,code=login},
