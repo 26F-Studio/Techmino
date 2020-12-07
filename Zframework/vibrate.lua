fbfe5857669bd26b3252ff9bce7dbbed855c16d0
MrZ626 <1046101471@qq.com>
2020-12-07 10:26:32 +0800

    整体下调震动强度

--- Zframework/vibrate.lua
+++ Zframework/vibrate.lua
@@ -1,8 +1,8 @@
-local level={0,0,.015,.02,.03,.04,.05,.06,.07,.08}
-local VIB=love.system.vibrate
+local level={0,0,.01,.015,.02,.03,.04,.05,.06,.07}
+local vib=love.system.vibrate
 return function(t)
 	local L=SETTING.vib
 	if L>0 then
-		VIB(level[L+t])
+		vib(level[L+t])
 	end
 end
