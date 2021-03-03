7587d8cafddd759a044ef7ed0a84ea4d21fa2004
MrZ626 <1046101471@qq.com>
2021-03-03 16:38:53 +0800

    IMG模块支持加载图片组

--- Zframework/image.lua
+++ Zframework/image.lua
@@ -12,13 +12,21 @@ function IMG.init(list)
 	local function load(skip)
 		local loaded=0
 		for k,v in next,list do
-			IMG[k]=love.graphics.newImage("media/image/"..v)
+			if type(v)=="string"then
+				IMG[k]=love.graphics.newImage("media/image/"..v)
+			else
+				for i=1,#v do
+					v[i]=love.graphics.newImage("media/image/"..v[i])
+				end
+				IMG[k]=v
+			end
 			loaded=loaded+1
 			if not skip and loaded~=count then
 				coroutine.yield()
 			end
 		end
 		IMG.loadOne=nil
+		IMG.loadAll=nil
 	end
 
 	IMG.loadOne=coroutine.wrap(load)
