416534b99c86afcf1b64155bd76168ef0fdc1fb2
MrZ626 <1046101471@qq.com>
2021-03-16 20:02:01 +0800

    调整四个backfire模式的各种参数

--- parts/modes/backfire_u.lua
+++ parts/modes/backfire_u.lua
@@ -1,14 +1,14 @@
-local format=string.format
 return{
-	color=COLOR.lGrey,
+	color=COLOR.lYellow,
 	env={
-		drop=30,lock=60,
+		drop=2,lock=30,
+		freshLimit=10,
 		dropPiece=function(P)
 			if P.lastPiece.atk>0 then
-				P:receive(nil,math.floor(P.lastPiece.atk*1.5),0,generateLine(P:RND(10)))
-				if P.stat.atk>=200 then
-					P:win("finish")
-				end
+				P:receive(nil,P.lastPiece.atk,0,generateLine(P:RND(10)))
+			end
+			if P.stat.atk>=100 then
+				P:win("finish")
 			end
 		end,
 		bg="blackhole",bgm="echo",
@@ -18,26 +18,26 @@ return{
 		PLY.newPlayer(1)
 	end,
 	mesDisp=function(P)
-		setFont(45)
-		mStr(format("%.1f",P.stat.atk),69,247)
-		mText(drawableText.atk,69,300)
+		setFont(65)
+		mStr(P.stat.atk,69,310)
+		mText(drawableText.atk,69,375)
 	end,
-	score=function(P)return{P.stat.atk<=200 and math.floor(P.stat.atk)or 200,P.stat.time}end,
+	score=function(P)return{P.stat.atk<=100 and math.floor(P.stat.atk)or 100,P.stat.atk}end,
 	scoreDisp=function(D)return D[1].." Attack  "..TIMESTR(D[2])end,
 	comp=function(a,b)return a[1]>b[1]or a[1]==b[1]and a[2]<b[2]end,
 	getRank=function(P)
 		local L=P.stat.atk
-		if L>=200 then
+		if L>=100 then
 			local T=P.stat.time
 			return
-			T<120 and 5 or
-			T<150 and 4 or
-			T<200 and 3 or
-			2
+			T<60 and 5 or
+			T<80 and 4 or
+			T<120 and 3 or
+			T<180 and 2 or
+			1
 		else
 			return
-			L>=126 and 1 or
-			L>=30 and 0
+			L>=50 and 0
 		end
 	end,
 }
