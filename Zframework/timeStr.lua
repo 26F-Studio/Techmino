8e382f104b784f4ae84c5cc16611fd7adfbec487
MrZ626 <1046101471@qq.com>
2021-03-17 21:08:02 +0800

    修改timeStr输出的时间字符串格式

--- Zframework/timeStr.lua
+++ Zframework/timeStr.lua
@@ -1,11 +1,11 @@
 local int,format=math.floor,string.format
 return function(s)
 	if s<60 then
-		return format("%.3fs",s)
+		return format("%.3fs\"",s)
 	elseif s<3600 then
-		return format("%d:%.2f",int(s/60),s%60)
+		return format("%d'%05.2f\"",int(s/60),s%60)
 	else
 		local h=int(s/3600)
-		return format("%d:%d:%.2f",h,int(s/60%60),s%60)
+		return format("%d:%.2d'%05.2f\"",h,int(s/60%60),s%60)
 	end
 end
