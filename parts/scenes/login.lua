2f8f27289410518f4ecd46c787101884351cce6e
MrZ626 <1046101471@qq.com>
2021-03-28 00:52:59 +0800

    登录界面问题修复

--- parts/scenes/login.lua
+++ parts/scenes/login.lua
@@ -7,7 +7,7 @@ local function login()
 		LOG.print(text.noPassword)return
 	end
 	USER.email=email
-	WS.send("user",JSON.encode{
+	WS.connect("user","/user",JSON.encode{
 		email=email,
 		password=password,
 	})
@@ -17,8 +17,8 @@ local scene={}
 
 scene.widgetList={
 	WIDGET.newText{name="title",		x=80,	y=50,font=70,align="L"},
-	-- WIDGET.newButton{name="register",	x=1140,	y=100,w=170,h=80,color="green",code=function()SCN.swapTo("register","swipeR")end},
-	WIDGET.newInputBox{name="email",		x=380,	y=200,w=500,h=60,regex="[0-9A-Za-z@._-]"},
+	-- WIDGET.newButton{name="register",x=1140,	y=100,w=170,h=80,color="green",code=function()SCN.swapTo("register","swipeR")end},
+	WIDGET.newInputBox{name="email",	x=380,	y=200,w=500,h=60,regex="[0-9A-Za-z@._-]"},
 	WIDGET.newInputBox{name="password",	x=380,	y=300,w=626,h=60,secret=true,regex="[ -~]"},
 	WIDGET.newKey{name="login",			x=1140,	y=540,w=170,h=80,font=40,code=login},
 	WIDGET.newButton{name="back",		x=1140,	y=640,w=170,h=80,font=40,code=backScene},
