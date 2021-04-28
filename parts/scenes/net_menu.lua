6d3ec2a8d2b853cf3bb89aaab9e07a91eec46ede
MrZ626 <1046101471@qq.com>
2021-04-29 00:16:42 +0800

    联网模式主菜单添加设置按钮

--- parts/scenes/net_menu.lua
+++ parts/scenes/net_menu.lua
@@ -15,10 +15,11 @@ function scene.draw()
 end
 
 scene.widgetList={
+	WIDGET.newKey{name="setting",fText=TEXTURE.setting,x=1200,y=160,w=90,h=90,code=pressKey"s"},
 	WIDGET.newButton{name="ffa",	x=640,	y=200,w=350,h=120,font=40,code=function()NET.enterRoom("ffa")end},
 	WIDGET.newButton{name="rooms",	x=640,	y=360,w=350,h=120,font=40,code=goScene"net_rooms"},
 	WIDGET.newButton{name="chat",	x=640,	y=540,w=350,h=120,color="D",font=40,code=NULL},
-	WIDGET.newButton{name="logout",	x=1180,	y=130,w=180,h=70,color="dR",
+	WIDGET.newButton{name="logout",	x=880,	y=40,w=180,h=60,color="dR",
 		code=function()
 			if TIME()-lastLogoutTime<1 then
 				if USER.uid then
