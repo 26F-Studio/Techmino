70179015942fe3f0fa4b76ea22bfbb0e7b8bc43e
MrZ626 <1046101471@qq.com>
2021-05-02 10:36:42 +0800

    修复地图点bigbang就爆炸

--- parts/modes/bigbang.lua
+++ parts/modes/bigbang.lua
@@ -11,7 +11,7 @@ return{
 		PLY.newPlayer(1)
 	end,
 	score=function(P)return{P.modeData.event,P.stat.finesseRate*20/P.stat.piece}end,
-	scoreDisp=function(D)return("%d Stage %.2f%"):format(D[1],D[2])end,
+	scoreDisp=function(D)return("%d Stage %.2f%%"):format(D[1],D[2])end,
 	comp=function(a,b)return a[1]>b[1]or a[1]==b[1]and a[2]>b[2]end,
 	getRank=function(P)
 		local W=P.modeData.event
