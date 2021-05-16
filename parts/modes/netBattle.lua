8636c6dcd5aeaf7274efef8157f43e807374b308
MrZ626 <1046101471@qq.com>
2021-05-16 14:09:06 +0800

    重新启用margin time，但基于帧而不是时间

--- parts/modes/netBattle.lua
+++ parts/modes/netBattle.lua
@@ -1,3 +1,4 @@
+local yield=YIELD
 return{
 	color=COLOR.white,
 	env={
@@ -6,6 +7,13 @@ return{
 		pushSpeed=5,
 		garbageSpeed=2,
 		allowMod=false,
+		task=function(P)
+			local S=P.stat
+			while true do yield()if S.frame>90*60 then P.strength=1;P:setFrameColor(1)break end end
+			while true do yield()if S.frame>135*60 then P.strength=2;P:setFrameColor(2)break end end
+			while true do yield()if S.frame>180*60 then P.strength=3;P:setFrameColor(3)break end end
+			while true do yield()if S.frame>260*60 then P.strength=4;P:setFrameColor(4)break end end
+		end,
 		bgm={'battle','cruelty','distortion','far','final','hope','magicblock','new era','push','race','rockblock','secret7th','secret8th','shining terminal','storm','super7th','warped','waterfall'},
 	},
 	load=function()
