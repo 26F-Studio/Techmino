d2b171427a5f1f7ab4c74d59b07b95b2d2fb62ed
MrZ626 <1046101471@qq.com>
2021-06-04 18:22:09 +0800

    提高无摩擦模式评级要求

--- parts/modes/sprintSmooth.lua
+++ parts/modes/sprintSmooth.lua
@@ -21,10 +21,10 @@ return{
 		if P.stat.row<40 then return end
 		local T=P.stat.time
 		return
-		T<=40 and 5 or
-		T<=50 and 4 or
-		T<=70 and 3 or
-		T<=100 and 2 or
+		T<=30 and 5 or
+		T<=45 and 4 or
+		T<=60 and 3 or
+		T<=90 and 2 or
 		T<=150 and 1 or
 		0
 	end,
