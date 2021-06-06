0a63bea195aa0a647f78395e0237926d16bf085a
MrZ626 <1046101471@qq.com>
2021-06-06 17:12:10 +0800

    修复开启3D场地后shader覆盖了贴图透明度（之前变量名打错了）

--- parts/shaders/lighter.glsl
+++ parts/shaders/lighter.glsl
@@ -4,6 +4,6 @@ vec4 effect(vec4 color,Image tex,vec2 tex_coords,vec2 scr_coords){
 		pow(texcolor.r+.26,.7023),
 		pow(texcolor.g+.26,.7023),
 		pow(texcolor.b+.26,.7023),
-		texcolor.b
+		texcolor.a
 	);
 }
