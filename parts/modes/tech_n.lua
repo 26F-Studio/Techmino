27ce516dd9f2d8a66621a4c3c9aa54ed488333e0
MrZ626 <1046101471@qq.com>
2021-06-22 20:39:47 +0800

    科研模式修改目标后评级代码未更新

--- parts/modes/tech_n.lua
+++ parts/modes/tech_n.lua
@@ -17,23 +17,22 @@ return{
 		mText(drawableText.atk,69,243)
 		mText(drawableText.eff,69,363)
 	end,
-	score=function(P)return{P.stat.atk<=200 and math.floor(P.stat.atk)or 200,P.stat.time}end,
+	score=function(P)return{P.stat.atk<=100 and math.floor(P.stat.atk)or 100,P.stat.time}end,
 	scoreDisp=function(D)return D[1].." Attack  "..STRING.time(D[2])end,
 	comp=function(a,b)return a[1]>b[1]or a[1]==b[1]and a[2]<b[2]end,
 	getRank=function(P)
-		local L=P.stat.atk
-		if L>=200 then
+		local A=P.stat.atk
+		if A>=100 then
 			local T=P.stat.time
 			return
-			T<120 and 5 or
-			T<150 and 4 or
-			3
+				T<50 and 5 or
+				T<70 and 4 or
+				T<100 and 3 or
+				2
 		else
 			return
-			L>=150 and 3 or
-			L>=100 and 2 or
-			L>=60 and 1 or
-			L>=20 and 0
+				A>=60 and 1 or
+				A>=30 and 0
 		end
 	end,
 }
