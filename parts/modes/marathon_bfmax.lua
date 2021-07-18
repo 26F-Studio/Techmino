41e58e0bd63e61e311d4f8daca9eaff7543256f6
MrZ626 <1046101471@qq.com>
2021-07-18 16:16:39 +0800

    修复marathon_bfmax死亡判定不合适 close #127

--- parts/modes/marathon_bfmax.lua
+++ parts/modes/marathon_bfmax.lua
@@ -8,22 +8,26 @@ return{
 		drop=60,wait=8,fall=20,
 		task=function(P)P.modeData.target=10 end,
 		dropPiece=function(P)
-			if P.combo>1 or P.b2b>0 or P.lastPiece.row>1 then
-				if P.combo>1 then 			P:showText("2x",0,-220,40,'flicker',.3)end
-				if P.b2b>0 then 			P:showText("spin",0,-160,40,'flicker',.3)end
-				if P.lastPiece.row>1 then 	P:showText("1+",0,-100,40,'flicker',.3)end
+			local flag
+			local l=P.lastPiece
+			if P.combo>1 then	flag=true;P:showText("2x",0,-220,40,'flicker',.3)end
+			if l.spin then		flag=true;P:showText("spin",0,-180,40,'flicker',.3)end
+			if l.row>1 then		flag=true;P:showText("1+",0,-140,40,'flicker',.3)end
+			if l.pc then		flag=true;P:showText("PC",0,-100,40,'flicker',.3)end
+			if l.hpc then		flag=true;P:showText("HPC",0,-100,40,'flicker',.3)end
+			if flag then
 				P:lose()
-				return
-			end
-			local T=P.modeData.target
-			if P.stat.row>=T then
-				if T==200 then
-					P:win('finish')
-				else
-					T=T+10
-					P.gameEnv.drop=dropSpeed[T/10]
-					P.modeData.target=T
-					SFX.play('reach')
+			else
+				local T=P.modeData.target
+				if P.stat.row>=T then
+					if T==200 then
+						P:win('finish')
+					else
+						T=T+10
+						P.gameEnv.drop=dropSpeed[T/10]
+						P.modeData.target=T
+						SFX.play('reach')
+					end
 				end
 			end
 		end,
