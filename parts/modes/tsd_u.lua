7407bbefdf8392c26e48fd80db84e57326f5c8a2
MrZ626 <1046101471@qq.com>
2021-08-03 00:37:49 +0800

    tsd_u中结束游戏的重复T2不计入成绩

--- parts/modes/tsd_u.lua
+++ parts/modes/tsd_u.lua
@@ -3,11 +3,11 @@ local function check_tsd(P)
 	local C=P.lastPiece
 	if C.row>0 then
 		if C.id==5 and C.row==2 and C.spin then
-			P.modeData.tsd=P.modeData.tsd+1
 			if TABLE.find(P.modeData.history,C.centX)then
 				P:showText("STACK",0,-140,40,'flicker',.3)
 				P:lose()
 			else
+				P.modeData.tsd=P.modeData.tsd+1
 				table.insert(P.modeData.history,1,C.centX)
 				P.modeData.history[5]=nil
 			end
