f088bdcf8b2e62ecf292f1b15e2c3fb7675374c1
MrZ626 <1046101471@qq.com>
2021-08-04 20:15:15 +0800

    修复松开鼠标可能不触发场景内事件

--- Zframework/init.lua
+++ Zframework/init.lua
@@ -149,10 +149,10 @@ end
 function love.mousereleased(x,y,k,touch)
 	if touch or SCN.swapping then return end
 	mx,my=ITP(xOy,x,y)
+	if SCN.mouseUp then SCN.mouseUp(mx,my,k)end
 	if WIDGET.sel then
 		WIDGET.release(mx,my)
 	else
-		if SCN.mouseUp then SCN.mouseUp(mx,my,k)end
 		if lastX and SCN.mouseClick and(mx-lastX)^2+(my-lastY)^2<62 then
 			SCN.mouseClick(mx,my,k)
 		end
