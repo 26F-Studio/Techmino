ac889dcba92a37d3f53427d2ee7d8ca43e53d3ab
MrZ626 <1046101471@qq.com>
2021-08-09 02:33:05 +0800

    修复进入游戏场景时初始化代码顺序错误

--- parts/scenes/game.lua
+++ parts/scenes/game.lua
@@ -70,18 +70,18 @@ local function _step()floatRepRate=floatRepRate+1 end
 
 
 function scene.sceneInit(org)
+	replaying=GAME.replaying
 	noKey=replaying
 	noTouch=not SETTING.VKSwitch or noKey
+	if org~='depause'and org~='pause'then
+		floatRepRate,replayRate=0,1
+	end
 
 	if GAME.init then
 		resetGameData()
 		GAME.init=false
 	end
-	replaying=GAME.replaying
 
-	if org~='depause'and org~='pause'then
-		floatRepRate,replayRate=0,1
-	end
 	updateRepButtons()
 	WIDGET.active.restart.hide=replaying
 	if SETTING.menuPos=='right'then
