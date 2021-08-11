b4a5a7e3202019bebe266b3fb5314a0a957d02dd
shoucandanghehe <wallfjjd@gmail.com>
2021-08-12 04:00:31 +0800

    win64&86 ci

--- .github/workflows/updateConfVersion.py
+++ .github/workflows/updateConfVersion.py
@@ -1,12 +1,15 @@
 import argparse
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="用于更新conf.lua内编译版本号")
-    parser.add_argument("-H", "--Hash", type=str, help = "Github提交Hash")
+    parser = argparse.ArgumentParser(description="用于更新conf.lua")
+    parser.add_argument("-H", "--Hash", type=str, default = False, help = "Github提交Hash")
     args = parser.parse_args()
     with open("conf.lua", "r+", encoding="utf-8") as file:
         data = file.read()
-        data = data.replace('@DEV', f'@{args.Hash[0:4]}')
+        if args.Hash != False:
+            data = data.replace('@DEV', f'@{args.Hash[0:4]}')
+        else:
+            data = data.replace('@DEV', '')
         file.seek(0)
         file.truncate()
         file.flush()
