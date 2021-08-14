adb205d8fab63474ba6b619d357798f4b8ae9a3a
MrZ626 <1046101471@qq.com>
2021-08-14 17:54:53 +0800

    移除堆积模式左侧信息

--- parts/modes/stack_40l.lua
+++ parts/modes/stack_40l.lua
@@ -20,11 +20,6 @@ return{
 		seqData={1,2,3,4,5,6,7,1,2,3,4,5,6,7,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25},
 		bg='blockrain',bgm='there',
 	},
-	mesDisp=function(P)
-		setFont(60)
-		mStr(P.stat.piece,63,280)
-		mText(drawableText.piece,63,350)
-	end,
 	score=function(P)return{getHoleCount(P),P.stat.time}end,
 	scoreDisp=function(D)return D[1].." Holes".."   "..STRING.time(D[2])end,
 	comp=function(a,b)return a[1]<b[1]or a[1]==b[1]and a[2]<b[2]end,
