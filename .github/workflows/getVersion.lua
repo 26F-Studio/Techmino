9f64d84c5a8c4c259201db303318dd14c23010e7
MrZ626 <1046101471@qq.com>
2021-08-17 23:44:48 +0800

    完善截取release note的格式转换

--- .github/workflows/getVersion.lua
+++ .github/workflows/getVersion.lua
@@ -1,12 +1,16 @@
 if arg[1]=="-code"then
-	print (require"version".apkCode)
+	print(require"version".apkCode)
 elseif arg[1]=="-string"then
-	print ((require"version".string:gsub("@DEV","")))
+	print((require"version".string:gsub("@DEV","")))
 elseif arg[1]=="-updateNote"then
 	local note=require"parts.updateLog"
 	local p1=note:find("\n%d")
 	local p2=note:find("\n%d",p1+1)
 	note=note:sub(p1,p2-2)
-	note=note:gsub("\t","")
-	print (note)
+	note=note:gsub("\t\t\t\t","_")
+	note=note:gsub("\t\t","")
+	note=note:gsub("\n([^_])","\n\n%1")
+	note=note:gsub("\n_","\n")
+	note=note:gsub("\n\n","\n",1)
+	print(note)
 end
