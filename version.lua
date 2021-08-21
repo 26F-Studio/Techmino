36c11961b6e4aa48080741684a9747c6a15284e6
MrZ626 <1046101471@qq.com>
2021-08-22 02:09:43 +0800

    修改更新历史版本号build号

--- version.lua
+++ version.lua
@@ -1,7 +1,7 @@
 return{
-	["apkCode"]=361,
-	["code"]=1601,
-	["string"]="V0.16.1@DEV",
+	["apkCode"]=362,
+	["code"]=1602,
+	["string"]="V0.16.2@DEV",
 	["room"]="V1.1",
-	["name"]="深空 Deep space",
+	["name"]="曲率引擎 Warp drive",
 }
