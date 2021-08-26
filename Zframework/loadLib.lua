a7f7374f96f8294331c9034c25b1147f32c6a817
MrZ626 <1046101471@qq.com>
2021-08-26 15:20:15 +0800

    修复loadlib小模块在安卓上会一直复制so库文件

--- Zframework/loadLib.lua
+++ Zframework/loadLib.lua
@@ -1,18 +1,22 @@
 package.cpath=package.cpath..';'..SAVEDIR..'/lib/lib?.so;'..'?.dylib'
+local loaded={}
 return function(libName)
     if SYSTEM=='Android'then
-        local platform=(function()
-            local p=io.popen('uname -m')
-            local arch=p:read('*a'):lower()
-            p:close()
-            return
-                arch=='aarch64'or arch=='arm64'and'arm64-v8a'or
-                'armeabi-v7a'
-        end)()
-        love.filesystem.write(
-            'lib/libCCloader.so',
-            love.filesystem.read('data','libAndroid/'..platform..'/libCCloader.so')
-        )
+        if not loaded[libName]then
+            local platform=(function()
+                local p=io.popen('uname -m')
+                local arch=p:read('*a'):lower()
+                p:close()
+                return
+                    arch=='aarch64'or arch=='arm64'and'arm64-v8a'or
+                    'armeabi-v7a'
+            end)()
+            love.filesystem.write(
+                'lib/libCCloader.so',
+                love.filesystem.read('data','libAndroid/'..platform..'/libCCloader.so')
+            )
+            loaded[libName]=true
+        end
     end
     local r1,r2,r3=pcall(require,libName)
     if r1 and r2 then
