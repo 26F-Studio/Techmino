fae14d4f9b6b503b039c61f4328d7daefebe4b25
MrZ626 <1046101471@qq.com>
2021-09-01 03:41:45 +0800

    修正联网游戏按钮位置偏了一点

--- parts/scenes/net_game.lua
+++ parts/scenes/net_game.lua
@@ -337,8 +337,8 @@ scene.widgetList={
     WIDGET.newKey{name="ready",   x=1060,y=510,w=360,h=90,color='lG',font=35,  code=_setReady,hideF=_hideF_ingame},
     WIDGET.newKey{name="spectate",x=1060,y=610,w=360,h=90,color='lO',font=35,  code=_setSpectate,hideF=_hideF_ingame},
     WIDGET.newKey{name="cancel",  x=1060,y=560,w=360,h=120,color='lH',font=40, code=_setCancel,hideF=_hideF_ingame2},
-    WIDGET.newKey{name="chat",    x=360,y=45,w=60,fText="...",font=35,         code=_switchChat},
-    WIDGET.newKey{name="quit",    x=860,y=45,w=60,fText=TEXTURE.quit_small,    code=_quit},
+    WIDGET.newKey{name="chat",    x=390,y=45,w=60,fText="...",font=35,         code=_switchChat},
+    WIDGET.newKey{name="quit",    x=890,y=45,w=60,fText=TEXTURE.quit_small,    code=_quit},
 }
 
 return scene
