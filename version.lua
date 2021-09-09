f67f31baa2c168cb6cf64436f92b9f076ab210e9
MrZ626 <1046101471@qq.com>
2021-09-09 21:10:17 +0800

    修改更新历史和build号和房间版本名

--- version.lua
+++ version.lua
@@ -1,7 +1,7 @@
 return{
-    ["apkCode"]=366,
+    ["apkCode"]=367,
     ["code"]=1602,
     ["string"]="V0.16.2@DEV",
-    ["room"]="V1.a",
+    ["room"]="ver A-0",
     ["name"]="曲率引擎 Warp drive",
 }
