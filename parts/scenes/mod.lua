b6e17594c60d27c7624c03e7eafc9591cf9c03bc
MrZ626 <1046101471@qq.com>
2021-09-11 16:32:25 +0800

    修正换回字体后mod菜单文本没对齐

--- parts/scenes/mod.lua
+++ parts/scenes/mod.lua
@@ -126,7 +126,7 @@ function scene.draw()
             gc.setColor(color)
             gc.circle('line',0,0,rad,side)
             gc.setColor(COLOR.Z)
-            mStr(M.id,0,-25)
+            mStr(M.id,0,-27)
             if M.sel>0 and M.list then
                 setFont(25)
                 gc.setColor(1,1,1,10*t)
