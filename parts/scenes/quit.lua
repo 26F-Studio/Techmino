dd30e4b3ece00f2bd0f76afb5e1a405fd9538e66
MrZ626 <1046101471@qq.com>
2021-09-15 01:22:48 +0800

    iOS退出失败会提示 close #273

--- parts/scenes/quit.lua
+++ parts/scenes/quit.lua
@@ -1,12 +1,24 @@
 local scene={}
 
 function scene.sceneInit()
-    if math.random()>.0000626 then
-        love.timer.sleep(.26)
-        love.event.quit()
+    if false and SYSTEM~="iOS"then
+        if math.random()>.0000626 then
+            love.timer.sleep(.26)
+            love.event.quit()
+        else
+            error("So lucky! 0.00626% to get this!!   You can quit the game now.")
+        end
     else
-        error("So lucky! 0.00626% to get this!!   You can quit the game now.")
+        MES.update(1e99)
+        SETTING.powerInfo=false
+        SETTING.tapFX=false
+        VERSION.string=""
+        MES.new('error',"Please quit with HOME button on iOS",1e99)
     end
+end
+
+function scene.draw()
+    love.graphics.clear()
 end
 
 return scene
