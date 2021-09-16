51a93ce88d759d051939086497d8c6e2c131e157
Not-A-Normal-Robot <76723917+Not-A-Normal-Robot@users.noreply.github.com>
2021-09-16 10:40:24 +0700

    fixed typos

--- parts/modes/rhythm_h.lua
+++ parts/modes/rhythm_h.lua
@@ -1,7 +1,7 @@
 return{
     color=COLOR.magenta,
     env={
-        mindas=7,minarr=1,misdarr=1,
+        mindas=7,minarr=1,minsdarr=1,
         keyCancel={6},
         eventSet='rhythm_h',
         bg='bg2',bgm='secret8th',
