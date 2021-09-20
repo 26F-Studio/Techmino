b5fca52b85405d4a61bccc2ed52f4342cd7f3fc0
Not-A-Normal-Robot <76723917+Not-A-Normal-Robot@users.noreply.github.com>
2021-09-21 00:58:17 +0700

    Add easier Classic modes (#302)
    * Remove SDARR limitation on Rhythm
    
    * Remove SDARR limitation on Rhythm
    
    * Remove SDARR limitation on Rhythm
    
    * Revert "Remove SDARR limitation on Rhythm"
    
    This reverts commit 1a0a56bbf692f56df864c226e609c5f01cc1c526.
    
    * Add classic modes: easy and normal
    
    (Easy: lvl9 start, Normal: lvl15 start)
    
    * fixed typos
    
    * Updated map

---
+++ parts/modes/classic_e.lua
@@ -0,0 +1,31 @@
+return{
+    color=COLOR.lBlue,
+    env={
+        center=0,ghost=0,
+        smooth=false,
+        face={0,0,2,2,2,0,0},
+        eventSet='classic_e',
+        bg='rgb',bgm='magicblock',
+    },
+    slowMark=true,
+    mesDisp=function(P)
+        setFont(75)
+        local r=P.modeData.target*.1
+        mStr(r<11 and 9 or r<22 and r or("%02x"):format(r*10-220),63,210)
+        mText(drawableText.speedLV,63,290)
+        PLY.draw.drawProgress(P.stat.row,P.modeData.target)
+    end,
+    score=function(P)return{P.stat.score,P.stat.row}end,
+    scoreDisp=function(D)return D[1].."   "..D[2].." Lines"end,
+    comp=function(a,b)return a[1]>b[1]or a[1]==b[1]and a[2]<b[2]end,
+    getRank=function(P)
+        local L=P.stat.row
+        return
+        L>=150 and 5 or
+        L>=120 and 4 or
+        L>=90 and 3 or
+        L>=60 and 2 or
+        L>=40 and 1 or
+        L>=10 and 0
+    end,
+}
