30ed4179d68266e307c5a7d3a1e65ee8a2c612f2
Not-A-Normal-Robot <76723917+Not-A-Normal-Robot@users.noreply.github.com>
2021-09-21 00:58:17 +0700

    Add easier Classic modes (#302)
    * Remove SDARR limitation on Rhythm
    
    * Remove SDARR limitation on Rhythm
    
    * Remove SDARR limitation on Rhythm
    
    * Revert "Remove SDARR limitation on Rhythm"
    
    This reverts commit 1a0a56bbf692f56df864c226e609c5f01cc1c526.
    
    * Add classic modes: easy and normal
    
    (Easy: lvl9 start, Normal: lvl15 start)
    
    * fixed typos
    
    * Updated map

---
+++ parts/eventsets/classic_n.lua
@@ -0,0 +1,48 @@
+return{
+    das=16,
+    arr=6,
+    sddas=2,
+    sdarr=2,
+    irs=false,
+    ims=false,
+    drop=4,
+    lock=4,
+    wait=10,
+    fall=25,
+    freshLimit=0,
+    fieldH=19,
+    nextCount=1,
+    holdCount=0,
+    RS='Classic',
+    sequence='rnd',
+    noTele=true,
+    keyCancel={5,6},
+    mesDisp=function(P)
+        setFont(75)
+        local r=P.modeData.target*.1
+        mStr(r<11 and 15 or r<22 and r+5 or("%02x"):format(r*10-220),63,210)
+        mText(drawableText.speedLV,63,290)
+        PLY.draw.drawProgress(P.stat.row,P.modeData.target)
+    end,
+    task=function(P)
+        P.modeData.target=10
+    end,
+    dropPiece=function(P)
+        local D=P.modeData
+        if P.stat.row>=D.target then
+            D.target=D.target+10
+            if D.target==110 then
+                P.gameEnv.drop,P.gameEnv.lock=3,3
+                SFX.play('blip_1')
+            elseif D.target==140 then
+                P.gameEnv.drop,P.gameEnv.lock=2,2
+                SFX.play('blip_1')
+            elseif D.target==240 then
+                P.gameEnv.drop,P.gameEnv.lock=1,1
+                SFX.play('blip_1')
+            else
+                SFX.play('reach')
+            end
+        end
+    end,
+}
