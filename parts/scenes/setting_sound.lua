b1a508f209cd48cba296094351a593342b5f7df9
MrZ626 <1046101471@qq.com>
2021-09-29 00:51:39 +0800

    微调声音设置ui

--- parts/scenes/setting_sound.lua
+++ parts/scenes/setting_sound.lua
@@ -80,13 +80,13 @@ scene.widgetList={
     WIDGET.newSlider{name="vib",      x=300, y=590,w=420,lim=220,color='lN',unit=10,change=function()VIB(2)end,disp=SETval('vib'),code=SETsto('vib')},
     WIDGET.newSlider{name="voc",      x=300, y=660,w=420,lim=220,color='lN',change=function()VOC.play('test')end,disp=SETval('voc'),code=SETsto('voc')},
 
-    WIDGET.newSwitch{name="autoMute", x=1150,y=210,lim=380,disp=SETval('autoMute'),code=SETrev('autoMute')},
-    WIDGET.newSwitch{name="fine",     x=1150,y=270,lim=380,disp=SETval('fine'),code=function()SETTING.fine=not SETTING.fine if SETTING.fine then SFX.play('finesseError',.6)end end},
+    WIDGET.newSwitch{name="autoMute", x=1150,y=180,lim=380,disp=SETval('autoMute'),code=SETrev('autoMute')},
+    WIDGET.newSwitch{name="fine",     x=1150,y=250,lim=380,disp=SETval('fine'),code=function()SETTING.fine=not SETTING.fine if SETTING.fine then SFX.play('finesseError',.6)end end},
 
-    WIDGET.newSelector{name="sfxPack",x=1100,y=340,w=200,color='lV',list={'chiptune'},disp=function()return sfxPack end,code=function(i)sfxPack=i end},
-    WIDGET.newButton{name="apply",    x=1100,y=410,w=180,h=60,code=function()SETTING.sfxPack=sfxPack SFX.load('media/effect/'..sfxPack..'/')end,hideF=function()return SETTING.sfxPack==sfxPack end},
-    WIDGET.newSelector{name="vocPack",x=1100,y=480,w=200,color='lV',list={'miya','mono','xiaoya','miku'},disp=function()return vocPack end,code=function(i)vocPack=i end},
-    WIDGET.newButton{name="apply",    x=1100,y=550,w=180,h=60,code=function()SETTING.vocPack=vocPack VOC.load('media/vocal/'..vocPack..'/')end,hideF=function()return SETTING.vocPack==vocPack end},
+    WIDGET.newSelector{name="sfxPack",x=1100,y=330,w=200,color='lV',list={'chiptune'},disp=function()return sfxPack end,code=function(i)sfxPack=i end},
+    WIDGET.newButton{name="apply",    x=1100,y=400,w=180,h=60,code=function()SETTING.sfxPack=sfxPack SFX.load('media/effect/'..sfxPack..'/')end,hideF=function()return SETTING.sfxPack==sfxPack end},
+    WIDGET.newSelector{name="vocPack",x=1100,y=470,w=200,color='lV',list={'miya','mono','xiaoya','miku'},disp=function()return vocPack end,code=function(i)vocPack=i end},
+    WIDGET.newButton{name="apply",    x=1100,y=540,w=180,h=60,code=function()SETTING.vocPack=vocPack VOC.load('media/vocal/'..vocPack..'/')end,hideF=function()return SETTING.vocPack==vocPack end},
     WIDGET.newButton{name="back",     x=1140,y=640,w=170,h=80,fText=TEXTURE.back,code=backScene},
 }
 
