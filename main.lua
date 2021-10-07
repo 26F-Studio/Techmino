958a296662398565542b38c2fea55e537b42e373
MrZ626 <1046101471@qq.com>
2021-10-07 18:49:57 +0800

    先临时恢复使用之前的字体文件等新字体确认没有问题

--- main.lua
+++ main.lua
@@ -45,7 +45,7 @@ end
 --Load modules
 Z=require'Zframework'
 
-FONT.init('parts/fonts/proportional.ttf')
+FONT.init('parts/fonts/puhui.ttf')
     setFont=FONT.set
     getFont=FONT.get
 SCR.setSize(1280,720)--Initialize Screen size
