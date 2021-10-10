956316c3277056aecfb1db1a27a8e6c2b6519c4a
MrZ626 <1046101471@qq.com>
2021-10-11 01:37:36 +0800

    生存模式和进攻模式添加胜利条件

--- parts/eventsets/survivor_e.lua
+++ parts/eventsets/survivor_e.lua
@@ -11,14 +11,18 @@ return{
                 local D=P.modeData
                 D.timer=D.timer+1
                 if D.timer>=math.max(60,150-2*D.wave)and P.atkBufferSum<4 then
-                    table.insert(P.atkBuffer,{line=generateLine(P.holeRND:random(10)),amount=1,countdown=30,cd0=30,time=0,sent=false,lv=1})
-                    P.atkBufferSum=P.atkBufferSum+1
-                    P.stat.recv=P.stat.recv+1
-                    if D.wave==45 then
-                        P:_showText(text.maxspeed,0,-140,100,'appear',.6)
+                    if D.wave==100 then
+                        P:win('finish')
+                    else
+                        table.insert(P.atkBuffer,{line=generateLine(P.holeRND:random(10)),amount=1,countdown=30,cd0=30,time=0,sent=false,lv=1})
+                        P.atkBufferSum=P.atkBufferSum+1
+                        P.stat.recv=P.stat.recv+1
+                        if D.wave==45 then
+                            P:_showText(text.maxspeed,0,-140,100,'appear',.6)
+                        end
+                        D.timer=0
+                        D.wave=D.wave+1
                     end
-                    D.timer=0
-                    D.wave=D.wave+1
                 end
             end
         end
--- parts/eventsets/survivor_h.lua
+++ parts/eventsets/survivor_h.lua
@@ -11,21 +11,25 @@ return{
                 local D=P.modeData
                 D.timer=D.timer+1
                 if D.timer>=math.max(60,180-2*D.wave)and P.atkBufferSum<15 then
-                    local s
-                    if D.wave%3<2 then
-                        table.insert(P.atkBuffer,{line=generateLine(P.holeRND:random(10)),amount=1,countdown=0,cd0=0,time=0,sent=false,lv=1})
-                        s=1
+                    if D.wave==90 then
+                        P:win('finish')
                     else
-                        table.insert(P.atkBuffer,{line=generateLine(P.holeRND:random(10)),amount=3,countdown=60,cd0=60,time=0,sent=false,lv=2})
-                        s=3
+                        local s
+                        if D.wave%3<2 then
+                            table.insert(P.atkBuffer,{line=generateLine(P.holeRND:random(10)),amount=1,countdown=0,cd0=0,time=0,sent=false,lv=1})
+                            s=1
+                        else
+                            table.insert(P.atkBuffer,{line=generateLine(P.holeRND:random(10)),amount=3,countdown=60,cd0=60,time=0,sent=false,lv=2})
+                            s=3
+                        end
+                        P.atkBufferSum=P.atkBufferSum+s
+                        P.stat.recv=P.stat.recv+s
+                        if D.wave==60 then
+                            P:_showText(text.maxspeed,0,-140,100,'appear',.6)
+                        end
+                        D.timer=0
+                        D.wave=D.wave+1
                     end
-                    P.atkBufferSum=P.atkBufferSum+s
-                    P.stat.recv=P.stat.recv+s
-                    if D.wave==60 then
-                        P:_showText(text.maxspeed,0,-140,100,'appear',.6)
-                    end
-                    D.timer=0
-                    D.wave=D.wave+1
                 end
             end
         end
