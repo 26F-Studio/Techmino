1ca5fd720421ab887fcc81dca7888ead8cf2e10b
MrZ626 <1046101471@qq.com>
2021-10-12 02:16:54 +0800

    添加一个无位置限制的消四模式

---
+++ parts/eventsets/techrash_n.lua
@@ -0,0 +1,12 @@
+return{
+    mesDisp=function(P)
+        setFont(60)
+        mStr(P.stat.clear[7][4],63,250)
+        mText(drawableText.techrash,63,315)
+    end,
+    dropPiece=function(P)
+        if P.lastPiece.row>0 and P.lastPiece.row<4 then
+            P:lose()
+        end
+    end,
+}
---
+++ parts/modes/techrash_n.lua
@@ -0,0 +1,20 @@
+return{
+    color=COLOR.lYellow,
+    env={
+        drop=60,lock=60,
+        freshLimit=15,
+        ospin=false,
+        eventSet='techrash_n',
+        bg='matrix',bgm='vapor',
+    },
+    getRank=function(P)
+        local T=P.stat.clear[7][4]
+        return
+        T>=100 and 5 or
+        T>=75 and 4 or
+        T>=50 and 3 or
+        T==30 and 2 or
+        T>=15 and 1 or
+        T>=5 and 0
+    end,
+}
