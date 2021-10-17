f3e18de45f06bdeb350af6a3ba2f910a6416062c
MrZ626 <1046101471@qq.com>
2021-10-18 00:28:12 +0800

    release note符合markdown语法，方便阅读

--- .github/workflows/getVersion.lua
+++ .github/workflows/getVersion.lua
@@ -18,11 +18,7 @@ elseif arg[1]=="-updateNote"then
     local p1=note:find("\n",note:find("\n%d")+1)+1
     local p2=note:find("\n%d",p1+1)
     note=note:sub(p1,p2-2)
-    note=note
-        :gsub("                ","_")
-        :gsub("        ","")
-        :gsub("\n([^_])","\n\n%1")
-        :gsub("\n_","\n")
-        :gsub("\n\n","\n",1)
+        :gsub("                ","- ")
+        :gsub("        ","# ")
     print(note)
 end
