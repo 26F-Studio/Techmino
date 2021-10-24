d5f01537f8d8e7740723220881f1b45ff5882397
MrZ626 <1046101471@qq.com>
2021-10-25 04:55:08 +0800

    iOS设备不显示收款码

--- parts/scenes/about.lua
+++ parts/scenes/about.lua
@@ -44,20 +44,22 @@ function scene.draw()
     gc.draw(TEXTURE.title,280,610,.1,.4+.03*sin(t*2.6),nil,580,118)
     gc.setLineWidth(3)
 
-    --QR Code frame
-    gc.rectangle('line',18,18,263,263)
-    gc.rectangle('line',1012,18,250,250)
+    if SYSTEM~='iOS'then
+        --QR Code frame
+        gc.rectangle('line',18,18,263,263)
+        gc.rectangle('line',1012,18,250,250)
 
-    --Support text
-    gc.setColor(1,1,1,sin(t*20)*.3+.6)
-    setFont(30)
-    mStr(text.support,150+sin(t*4)*20,283)
-    mStr(text.support,1138-sin(t*4)*20,270)
+        --Support text
+        gc.setColor(1,1,1,sin(t*20)*.3+.6)
+        setFont(30)
+        mStr(text.support,150+sin(t*4)*20,283)
+        mStr(text.support,1138-sin(t*4)*20,270)
+    end
 end
 
 scene.widgetList={
-    WIDGET.newImage{name="pay1",    x=20,  y=20},
-    WIDGET.newImage{name="pay2",    x=1014,y=20},
+    WIDGET.newImage{name="pay1",    x=20,  y=20,hide=SYSTEM=='iOS'},
+    WIDGET.newImage{name="pay2",    x=1014,y=20,hide=SYSTEM=='iOS'},
     WIDGET.newButton{name="staff",  x=1140,y=400,w=220,h=80,font=35,code=goScene'staff'},
     WIDGET.newButton{name="his",    x=1140,y=500,w=220,h=80,font=35,code=goScene'history'},
     WIDGET.newButton{name="qq",     x=1140,y=600,w=220,h=80,font=35,code=function()love.system.openURL("tencent://message/?uin=1046101471&Site=&Menu=yes")end,hide=MOBILE},
