6029363af8587a7e12e66d50a0e3eb4e3615c157
MrZ626 <1046101471@qq.com>
2021-10-25 03:50:30 +0800

    微调launchpad的ui

--- parts/scenes/launchpad.lua
+++ parts/scenes/launchpad.lua
@@ -201,11 +201,11 @@ function scene.draw()
         if showLabel then
             if k.sfx then mStr(k.sfx,x*80+40,y*80-30)gc_circle('fill',x*80+40,(y-1)*80+40,6)end
             if k.voc then mStr(k.voc,x*80+40,y*80-17)gc_rectangle('line',x*80+30,(y-1)*80+30,20,20,1)end
-            if k.bgm then mStr(k.bgm,x*80+40,y*80-76)gc_rectangle('fill',x*80+20,(y-1)*80+15,50,5,2)end
+            if k.bgm then mStr(k.bgm,x*80+40,y*80-78)gc_rectangle('fill',x*80+20,(y-1)*80+15,40,5,2)end
         else
             if k.sfx then gc_circle('fill',x*80+40,(y-1)*80+40,6)end
             if k.voc then gc_rectangle('line',x*80+30,(y-1)*80+30,20,20,1)end
-            if k.bgm then gc_rectangle('fill',x*80+20,(y-1)*80+15,50,5,2)end
+            if k.bgm then gc_rectangle('fill',x*80+20,(y-1)*80+15,40,5,2)end
         end
         if pad.alpha[y][x]>0 then
             gc_setColor(1,1,1,pad.alpha[y][x]*.7)
