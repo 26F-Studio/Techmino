85d72a1ee581c8baabe6b49e8d2a389044e3adf0
MrZ626 <1046101471@qq.com>
2021-10-26 11:04:21 +0800

    再微调wing背景

--- parts/backgrounds/wing.lua
+++ parts/backgrounds/wing.lua
@@ -13,7 +13,7 @@ local wingColor={
     {.5,.5,1.,.3},
     {.3,.8,.9,.3},
 }
-local wingHeight={.572,.536,.472,.405,.307,.402,.457,.367}
+local wingHeight={.572,.536,.476,.405,.307,.402,.457,.367}
 function back.init()
     crystal_img=GC.DO{42,118,
         {'setCL',.93,.93,.93},
@@ -31,7 +31,7 @@ function back.resize()
     for i=1,16 do
         crystals[i]={
             x=i<9 and SCR.w*.05*i or SCR.w*.05*(28-i),
-            y=SCR.h*(wingHeight[i]or wingHeight[i-8]),
+            y=SCR.h*(wingHeight[i<9 and i or i-8]),
             a=0,
             va=0,
             f=i<9 and .012-i*.0005 or .012-(17-i)*.0005
@@ -47,20 +47,14 @@ function back.update()
 end
 function back.draw()
     gc.clear(.06,.06,.06)
-    gc.setColor(.12,.12,.12)
-    gc.setLineWidth(0.02*SCR.h)
-    gc.line(0.018*SCR.w,0.567*SCR.h,0.101*SCR.w,0.512*SCR.h)
-    gc.line(0.202*SCR.w,0.369*SCR.h,0.099*SCR.w,0.514*SCR.h)
-    gc.line(0.201*SCR.w,0.371*SCR.h,0.260*SCR.w,0.212*SCR.h)
-    gc.line(0.247*SCR.w,0.257*SCR.h,0.307*SCR.w,0.383*SCR.h)
-    gc.line(0.352*SCR.w,0.436*SCR.h,0.305*SCR.w,0.381*SCR.h)
-    gc.line(0.344*SCR.w,0.437*SCR.h,0.401*SCR.w,0.307*SCR.h)
-    gc.line(0.982*SCR.w,0.567*SCR.h,0.899*SCR.w,0.512*SCR.h)
-    gc.line(0.798*SCR.w,0.369*SCR.h,0.901*SCR.w,0.514*SCR.h)
-    gc.line(0.799*SCR.w,0.371*SCR.h,0.740*SCR.w,0.212*SCR.h)
-    gc.line(0.753*SCR.w,0.257*SCR.h,0.693*SCR.w,0.383*SCR.h)
-    gc.line(0.648*SCR.w,0.436*SCR.h,0.695*SCR.w,0.381*SCR.h)
-    gc.line(0.656*SCR.w,0.437*SCR.h,0.599*SCR.w,0.307*SCR.h)
+    gc.setColor(.12,.10,.08)
+    gc.setLineJoin('bevel')
+    gc.setLineWidth(14*SCR.k)
+    local W,H=SCR.w,SCR.h
+    gc.line(.018*W,.567*H,.101*W,.512*H,.202*W,.369*H,.260*W,.212*H)
+    gc.line(.247*W,.257*H,.307*W,.383*H,.352*W,.436*H,.401*W,.309*H)
+    gc.line(.982*W,.567*H,.899*W,.512*H,.798*W,.369*H,.740*W,.212*H)
+    gc.line(.753*W,.257*H,.693*W,.383*H,.648*W,.436*H,.599*W,.309*H)
 
     local k=SCR.k
     for i=1,8 do
