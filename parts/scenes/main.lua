72d4faa52ac89e8b7e47999a10de33c0feec407a
MrZ626 <1046101471@qq.com>
2021-11-01 21:56:57 +0800

    微调进控制台音效力度

--- parts/scenes/main.lua
+++ parts/scenes/main.lua
@@ -14,10 +14,10 @@ local widgetX0={
 }
 local enterConsole=coroutine.wrap(function()
     while true do
-        Snd('bell',.3,'A4',.5,'E5',1,coin('A5','B5'))YIELD()
-        Snd('bell',.3,'A4',.5,'F5',1,coin('C6','D6'))YIELD()
-        Snd('bell',.3,'A4',.5,'G5',1,coin('E6','G6'))YIELD()
-        Snd('bell',.3,'A4',.5,'A5',1,'A6')SFX.play('ren_mega')SCN.go('app_console')YIELD()
+        Snd('bell',.6,'A4',.7,'E5',1,coin('A5','B5'))YIELD()
+        Snd('bell',.6,'A4',.7,'F5',1,coin('C6','D6'))YIELD()
+        Snd('bell',.6,'A4',.7,'G5',1,coin('E6','G6'))YIELD()
+        Snd('bell',.6,'A4',.7,'A5',1,'A6')SFX.play('ren_mega')SCN.go('app_console')YIELD()
     end
 end)
 function scene.sceneInit()
