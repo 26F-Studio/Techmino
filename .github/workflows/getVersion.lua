bedfa74a327ef2cca2fec559c32c03b950222cd8
MrZ626 <1046101471@qq.com>
2021-11-02 19:34:46 +0800

    整理代码

--- .github/workflows/getVersion.lua
+++ .github/workflows/getVersion.lua
@@ -1,22 +1,23 @@
-if arg[1]=="-apkCode"then
+local arg=arg[1]
+if arg=="-apkCode"then
     local code=require"version".apkCode
     print(code)
-elseif arg[1]=="-code"then
+elseif arg=="-code"then
     local str=require"version".code
     print(str)
-elseif arg[1]=="-name"then
+elseif arg=="-name"then
     local str=require"version".string
     print(str)
-elseif arg[1]=="-release"then
+elseif arg=="-release"then
     local str=require"version".string:gsub("V","",1)
     print(str)
-elseif arg[1]=="-updateTitle"then
+elseif arg=="-updateTitle"then
     local note=require"parts.updateLog"
     local p1=note:find("\n%d")+1
     local p2=note:find("\n",p1)-1
     note=note:sub(p1,p2)
     print(note)
-elseif arg[1]=="-updateNote"then
+elseif arg=="-updateNote"then
     local note=require"parts.updateLog"
     local p1=note:find("\n",note:find("\n%d")+1)+1
     local p2=note:find("\n%d",p1+1)
