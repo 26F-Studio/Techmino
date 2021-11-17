3efa646ee3e309abfb5a92db0964520fb8a7dc43
MrZ626 <1046101471@qq.com>
2021-11-17 22:11:50 +0800

    再增加堆积模式的窒息延迟

--- parts/modes/stack_e.lua
+++ parts/modes/stack_e.lua
@@ -2,7 +2,7 @@ return{
     env={
         drop=60,lock=60,
         wait=0,fall=50,
-        hang=8,
+        hang=15,
         garbageSpeed=30,
         seqData={1,2,3,4,5,6,7},
         eventSet='stack_e',
