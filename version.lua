8e674e3e290015519c20c09d997d14cbf43ee804
MrZ626 <1046101471@qq.com>
2021-11-17 16:25:38 +0800

    版本推进

--- version.lua
+++ version.lua
@@ -1,7 +1,7 @@
 return{
-    ["apkCode"]=401,
-    ["code"]=1605,
-    ["string"]="V0.16.5",
+    ["apkCode"]=402,
+    ["code"]=1700,
+    ["string"]="V0.17.0",
     ["room"]="ver A-0",
     ["name"]="新世界 New World",
 }
