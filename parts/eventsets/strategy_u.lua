1670c6e7d639d12b3778960efe8978956a9644cb
NOT_A_ROBOT <76723917+Not-A-Normal-Robot@users.noreply.github.com>
2021-11-18 18:06:17 +0700

    Add strategy modes (#468)
    * Add strategy mode

---
+++ parts/eventsets/strategy_u.lua
@@ -0,0 +1,31 @@
+local waitSpeed={15,15,14,14,13,13,12,12,11,11,10,10,9,9,8,8,7,7,7}
+
+return
+{
+    drop=0,
+    wait=15,
+    fall=0,
+	lock=5,
+	freshLimit=12,
+    mesDisp=function(P)
+        PLY.draw.drawProgress(P.stat.row,P.modeData.target)
+        PLY.draw.drawTargetLine(P,200-P.stat.row)
+    end,
+    task=function(P)
+        P.modeData.target=10
+    end,
+    hook_drop=function(P)
+        if P.stat.row>=P.modeData.target then
+            if P.modeData.target==200 then
+                P:win('finish')
+            else
+				if P.modeData.target==100 then
+					P.modeData.lock=4
+				end
+                P.gameEnv.wait=waitSpeed[P.modeData.target/10]
+                P.modeData.target=P.modeData.target+10
+                SFX.play('reach')
+            end
+        end
+    end
+}
---
+++ parts/modes/strategy_e.lua
@@ -0,0 +1,22 @@
+return{
+    env={
+        das=5,arr=1,
+        sequence="bagES",
+        eventSet='strategy_e',
+        bg='bg2',bgm='push',
+    },
+    slowMark=true,
+    score=function(P)return{math.min(P.stat.row,200),P.stat.time}end,
+    scoreDisp=function(D)return D[1].." Lines   "..STRING.time(D[2])end,
+    comp=function(a,b)return a[1]>b[1]or a[1]==b[1]and a[2]<b[2]end,
+    getRank=function(P)
+        local L=P.stat.row
+        return
+		L>=200 and 5 or
+		L>=170 and 4 or
+		L>=150 and 3 or
+        L>=120 and 2 or
+        L>=60 and 1 or
+        L>=26 and 0
+    end,
+}
