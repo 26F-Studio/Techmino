5f7a3fd53f19416291a0739f743e94d650456ebd
MrZ626 <1046101471@qq.com>
2021-11-18 19:54:31 +0800

    版本推进

--- version.lua
+++ version.lua
@@ -1,7 +1,7 @@
 return{
-    ["apkCode"]=403,
+    ["apkCode"]=404,
     ["code"]=1700,
     ["string"]="V0.17.0",
     ["room"]="ver A-0",
-    ["name"]="新世界 New World",
+    ["name"]="硬着陆 Hard Landing",
 }
