077c651226e348f16f31024a4c37c8c83a07c816
MrZ626 <1046101471@qq.com>
2021-11-29 01:16:22 +0800

    微调键位设置菜单

--- parts/scenes/setting_key.lua
+++ parts/scenes/setting_key.lua
@@ -95,7 +95,7 @@ function scene.draw()
             local font=#key[2]==1 and 40 or #key[2]<6 and 30 or 15
             setFont(font)
             mStr(key,
-                (i>10 and 940 or 210)+100*j,
+                (i>10 and 930 or 200)+100*j,
                 (
                     i>10 and 60*(i-10)-23 or
                     i>0 and 60*i-23 or
@@ -126,27 +126,27 @@ local function _setSel(i)
     end
 end
 scene.widgetList={
-    WIDGET.newKey{name='a1',x=160,y=40,w=200,h=60,code=function()_setSel(1)end},
-    WIDGET.newKey{name='a2',x=160,y=100,w=200,h=60,code=function()_setSel(2)end},
-    WIDGET.newKey{name='a3',x=160,y=160,w=200,h=60,code=function()_setSel(3)end},
-    WIDGET.newKey{name='a4',x=160,y=220,w=200,h=60,code=function()_setSel(4)end},
-    WIDGET.newKey{name='a5',x=160,y=280,w=200,h=60,code=function()_setSel(5)end},
-    WIDGET.newKey{name='a6',x=160,y=340,w=200,h=60,code=function()_setSel(6)end},
-    WIDGET.newKey{name='a7',x=160,y=400,w=200,h=60,code=function()_setSel(7)end},
-    WIDGET.newKey{name='a8',x=160,y=460,w=200,h=60,code=function()_setSel(8)end},
-    WIDGET.newKey{name='a9',x=160,y=520,w=200,h=60,code=function()_setSel(9)end},
-    WIDGET.newKey{name='a10',x=160,y=580,w=200,h=60,code=function()_setSel(10)end},
+    WIDGET.newKey{name='a1',x=150,y=40,w=180,h=60,font=25,code=function()_setSel(1)end},
+    WIDGET.newKey{name='a2',x=150,y=100,w=180,h=60,font=25,code=function()_setSel(2)end},
+    WIDGET.newKey{name='a3',x=150,y=160,w=180,h=60,font=25,code=function()_setSel(3)end},
+    WIDGET.newKey{name='a4',x=150,y=220,w=180,h=60,font=25,code=function()_setSel(4)end},
+    WIDGET.newKey{name='a5',x=150,y=280,w=180,h=60,font=25,code=function()_setSel(5)end},
+    WIDGET.newKey{name='a6',x=150,y=340,w=180,h=60,font=25,code=function()_setSel(6)end},
+    WIDGET.newKey{name='a7',x=150,y=400,w=180,h=60,font=25,code=function()_setSel(7)end},
+    WIDGET.newKey{name='a8',x=150,y=460,w=180,h=60,font=25,code=function()_setSel(8)end},
+    WIDGET.newKey{name='a9',x=150,y=520,w=180,h=60,font=25,code=function()_setSel(9)end},
+    WIDGET.newKey{name='a10',x=150,y=580,w=180,h=60,font=25,code=function()_setSel(10)end},
 
-    WIDGET.newKey{name='a11',x=800,y=40,w=200,h=60,code=function()_setSel(11)end},
-    WIDGET.newKey{name='a12',x=800,y=100,w=200,h=60,code=function()_setSel(12)end},
-    WIDGET.newKey{name='a13',x=800,y=160,w=200,h=60,code=function()_setSel(13)end},
-    WIDGET.newKey{name='a14',x=800,y=220,w=200,h=60,code=function()_setSel(14)end},
-    WIDGET.newKey{name='a15',x=800,y=280,w=200,h=60,code=function()_setSel(15)end},
-    WIDGET.newKey{name='a16',x=800,y=340,w=200,h=60,code=function()_setSel(16)end},
-    WIDGET.newKey{name='a17',x=800,y=400,w=200,h=60,code=function()_setSel(17)end},
-    WIDGET.newKey{name='a18',x=800,y=460,w=200,h=60,code=function()_setSel(18)end},
-    WIDGET.newKey{name='a19',x=800,y=520,w=200,h=60,code=function()_setSel(19)end},
-    WIDGET.newKey{name='a20',x=800,y=580,w=200,h=60,code=function()_setSel(20)end},
+    WIDGET.newKey{name='a11',x=790,y=40,w=180,h=60,font=25,code=function()_setSel(11)end},
+    WIDGET.newKey{name='a12',x=790,y=100,w=180,h=60,font=25,code=function()_setSel(12)end},
+    WIDGET.newKey{name='a13',x=790,y=160,w=180,h=60,font=25,code=function()_setSel(13)end},
+    WIDGET.newKey{name='a14',x=790,y=220,w=180,h=60,font=25,code=function()_setSel(14)end},
+    WIDGET.newKey{name='a15',x=790,y=280,w=180,h=60,font=25,code=function()_setSel(15)end},
+    WIDGET.newKey{name='a16',x=790,y=340,w=180,h=60,font=25,code=function()_setSel(16)end},
+    WIDGET.newKey{name='a17',x=790,y=400,w=180,h=60,font=25,code=function()_setSel(17)end},
+    WIDGET.newKey{name='a18',x=790,y=460,w=180,h=60,font=25,code=function()_setSel(18)end},
+    WIDGET.newKey{name='a19',x=790,y=520,w=180,h=60,font=25,code=function()_setSel(19)end},
+    WIDGET.newKey{name='a20',x=790,y=580,w=180,h=60,font=25,code=function()_setSel(20)end},
 
     WIDGET.newKey{name='restart',x=160,y=670,w=200,h=60,code=function()_setSel(0)end},
 
