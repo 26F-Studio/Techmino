e44b18943823aa96dc1b6fb15effb9cd0e56a0b7
emailadjie@gmail.com <76723917+Not-A-Normal-Robot@users.noreply.github.com>
2021-11-30 09:46:37 +0700

    Add Big Mode
    I even halved the gravity :)

---
+++ parts/eventsets/big_n.lua
@@ -0,0 +1,32 @@
+local dropSpeed={100,80,60,48,36,28,20,16,12,10,8,6,4,2,2,1,1,.5,.5}
+
+return
+{
+    drop=120,
+    wait=8,
+    fall=20,
+    mesDisp=function(P)
+        PLY.draw.drawProgress(P.stat.row,P.modeData.target)
+        PLY.draw.drawTargetLine(P,200-P.stat.row)
+    end,
+    task=function(P)
+		local F=P.field
+        for i=1,24 do
+            F[i]=LINE.new(20)
+            P.visTime[i]=LINE.new(20)
+            for x=3,7 do F[i][x]=0 end
+        end
+        P.modeData.target=10
+    end,
+    hook_drop=function(P)
+        if P.stat.row>=P.modeData.target then
+            if P.modeData.target==200 then
+                P:win('finish')
+            else
+                P.gameEnv.drop=dropSpeed[P.modeData.target/10]
+                P.modeData.target=P.modeData.target+10
+                SFX.play('reach')
+            end
+        end
+    end
+}
---
+++ parts/modes/big_n.lua
@@ -0,0 +1,28 @@
+return{
+    env={
+        noTele=true,
+        mindas=7,minarr=1,minsdarr=1,
+        sequence="bagES",
+        hook_drop=require'parts.eventsets.bigWallGen'.hook_drop,
+		eventSet='big_n',
+        bg='bg2',bgm='push',
+    },
+    score=function(P)return{math.min(P.modeData.maxCombo,100),P.stat.time}end,
+    scoreDisp=function(D)return D[1].." Combo   "..STRING.time(D[2])end,
+    comp=function(a,b)return a[1]>b[1]or a[1]==b[1]and a[2]<b[2]end,
+    getRank=function(P)
+        local L=P.stat.row
+        if L>=200 then
+            local T=P.stat.time
+            return
+            T<=180 and 5 or
+            T<=300 and 4 or
+            3
+        else
+            return
+            L>=150 and 2 or
+            L>=100 and 1 or
+            L>=20 and 0
+        end
+    end,
+}
