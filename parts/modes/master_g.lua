89134d4f076855d852182c1bc1f6da5e53f075a4
MrZ_26 <1046101471@qq.com>
2022-01-19 02:41:51 +0800

    整理刚增加的模式的代码

--- parts/modes/master_g.lua
+++ parts/modes/master_g.lua
@@ -1,22 +1,21 @@
 return{
     env={
         freshLimit=15,
+        fieldH=19,
         sequence="bagES",
         eventSet='master_g',
         bg='bg2',bgm='secret7th',
     },
     slowMark=true,
     score=function(P)
-		if not tonumber(P.modeData.grade) then
-			return{P.modeData.grade,P.stat.time}
-		else
-			return{"Grade "..tostring(P.modeData.grade),P.stat.time}
-		end 
-	end,
-    scoreDisp=function(D) return D[1].."   "..STRING.time(D[2])end,
-    comp=function(a,b)
-        return a[1]>b[1]or(a[1]==b[1]and a[2]<b[2])
+        if not tonumber(P.modeData.grade) then
+            return{P.modeData.grade,P.stat.time}
+        else
+            return{"Grade "..tostring(P.modeData.grade),P.stat.time}
+        end
     end,
+    scoreDisp=function(D) return D[1].."   "..STRING.time(D[2])end,
+    comp=function(a,b)return a[1]>b[1]or(a[1]==b[1]and a[2]<b[2])end,
     getRank=function(P)
         local G=P.modeData.gradePts
         return
@@ -25,10 +24,10 @@ return{
             G>=25 and 3 or -- m8 - MO
             G>=18 and 2 or -- m1-m7
             G>=9 and 1 or -- S1-S9
-			0
-			
-			-- Table of grades vs values
-			-- 9 8 7 6 5 4 3 2 1 S1 S2 S3 S4 S5 S6 S7 S8 S9 m1 m2 m3 m4 m5 m6 m7 m8 m9  M MK MV MO MM- MM MM+ GM- GM GM+ TM- TM TM+
-			-- 0 1 2 3 4 5 6 7 8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31  32 33  34  35 36  37  38 39
+            0
+
+            -- Table of grades vs values
+            -- 9 8 7 6 5 4 3 2 1 S1 S2 S3 S4 S5 S6 S7 S8 S9 m1 m2 m3 m4 m5 m6 m7 m8 m9  M MK MV MO MM- MM MM+ GM- GM GM+ TM- TM TM+
+            -- 0 1 2 3 4 5 6 7 8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31  32 33  34  35 36  37  38 39
     end,
 }
