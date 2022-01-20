a7a2124bb660bdb49f6bdfb7f3f8381fb13ec9a8
MrZ_26 <1046101471@qq.com>
2022-01-20 13:27:23 +0800

    版本推进

--- version.lua
+++ version.lua
@@ -1,7 +1,7 @@
 return{
-    ["apkCode"]=430,
+    ["apkCode"]=431,
     ["code"]=1702,
     ["string"]="V0.17.2",
-    ["room"]="ver A-5",
-    ["name"]="",
+    ["room"]="ver A-6",
+    ["name"]="0.17.2",
 }
