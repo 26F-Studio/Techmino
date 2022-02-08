9031fc1b63acaca5d829e9e73c3c4e9b3c76f356
NOT_A_ROBOT <76723917+Not-A-Normal-Robot@users.noreply.github.com>
2022-02-08 21:27:37 +0700

    added score counter for classic modes
    note: doesn't take into account softdrop

--- parts/eventsets/classic_l.lua
+++ parts/eventsets/classic_l.lua
@@ -1,20 +1,39 @@
-local gc_setColor=love.graphics.setColor
+local gc=love.graphics
+local gc_setColor=gc.setColor
 return{
     das=16,arr=6,
 	sddas=2,sdarr=2,
     irs=false,ims=false,
-    drop=2,
-    lock=2,
-    wait=10,
-    fall=25,
+    drop=2,lock=2,
+    wait=10,fall=25,
     freshLimit=0,
     fieldH=19,
-    nextCount=1,holdCount=0,
+    nextCount=1,
+    holdCount=0,
     RS='Classic',
     sequence='rnd',
     noTele=true,
     keyCancel={5,6},
     mesDisp=function(P)
+        gc.setLineWidth(2)
+        gc_setColor(.98,.98,.98,.8)
+        gc.rectangle('line',-100,0,200,100,4)
+        gc_setColor(.98,.98,.98,.4)
+        gc.rectangle('fill',-100+2,0+2,200-4,100-4,2)
+        gc_setColor(1,1,1,1)
+        setFont(40)
+        mStr("Score",0,0)
+        gc_setColor(.6,.6,.6,1)
+        local score_str=""
+        local score_length=math.floor(math.log10(P.modeData.gameScore))
+        for i=7,0,-1 do
+            score_str=score_str..(math.floor(P.modeData.gameScore/(math.pow(10,i)))%10)
+        end
+        for i=1,8 do
+            if score_length==-i+8 then gc_setColor(1,1,1,1) end
+            mStr(string.sub(score_str,i,i),25*i-113,50)
+        end
+        gc_setColor(1,1,1,1)
         setFont(75)
         local r=P.modeData.target/10
         mStr(r<11 and 19 or r<22 and r+9 or("%02x"):format(r*10-220),63,210)
@@ -34,6 +53,7 @@ return{
     end,
     task=function(P)
         P.modeData.target=10
+        P.modeData.gameScore=0
     end,
     hook_drop=function(P)
         local D=P.modeData
@@ -43,9 +63,19 @@ return{
             if D.target==200 then
                 P.gameEnv.drop,P.gameEnv.lock=1,1
                 SFX.play('blip_1')
+            elseif D.target==300 then
+                P:win('finish')
             else
                 SFX.play('reach')
             end
+            D.target=D.target>=300 and 300 or D.target+10
+            local r=D.target/10
+            local l=#P.clearedRow
+            P.modeData.gameScore=P.modeData.gameScore+((l==1 and 40 or l==2 and 100 or l==3 and 300 or 1200)*((r<11 and 19 or r<22 and r+9)+1))
+        elseif #P.clearedRow>0 then
+            local r=D.target/10
+            local l=#P.clearedRow
+            P.modeData.gameScore=P.modeData.gameScore+((l==1 and 40 or l==2 and 100 or l==3 and 300 or 1200)*((r<11 and 19 or r<22 and r+9)+1))
         end
     end,
 }
