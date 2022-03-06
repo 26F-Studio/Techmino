b44299ed271219806ffe764b1f959fe79dd8991c
NOT_A_ROBOT <76723917+Not-A-Normal-Robot@users.noreply.github.com>
2022-03-06 18:41:24 +0700

    Added efficiency-based Dig modes

---
+++ parts/modes/dig_eff_10l.lua
@@ -0,0 +1,21 @@
+return{
+    env={
+        pushSpeed=6,
+        eventSet='dig_10l',
+        bg='bg1',bgm='way',
+    },
+    score=function(P)return{P.stat.time,P.stat.piece}end,
+    scoreDisp=function(D)return STRING.time(D[1]).."   "..D[2].." Pieces"end,
+    comp=function(a,b)return a[1]<b[1]or a[1]==b[1]and a[2]<b[2]end,
+    getRank=function(P)
+        if P.stat.dig<10 then return end
+        local P=P.stat.piece
+        return
+        P<=12 and 5 or
+        P<=18 and 4 or
+        P<=25 and 3 or
+        P<=30 and 2 or
+        P<=40 and 1 or
+        0
+    end,
+}
