4f4988d7efe97962e6947ac6d0deb247afb3d152
NOT_A_ROBOT <76723917+Not-A-Normal-Robot@users.noreply.github.com>
2022-03-08 16:55:48 +0700

    Fix incorrect score order in Dig Efficiency modes

--- parts/modes/dig_eff_100l.lua
+++ parts/modes/dig_eff_100l.lua
@@ -4,8 +4,8 @@ return{
         eventSet='dig_100l',
         bg='bg2',bgm='way',
     },
-    score=function(P)return{P.stat.time,P.stat.piece}end,
-    scoreDisp=function(D)return STRING.time(D[1]).."   "..D[2].." Pieces"end,
+    score=function(P)return{P.stat.piece,P.stat.time}end,
+    scoreDisp=function(D)return D[1].." Pieces   "..STRING.time(D[2]) end,
     comp=function(a,b)return a[1]<b[1]or a[1]==b[1]and a[2]<b[2]end,
     getRank=function(P)
         if P.stat.dig<100 then return end
