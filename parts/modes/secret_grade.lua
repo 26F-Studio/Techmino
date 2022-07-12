5459c6a7db53383edb5509b2ae3207e6de7939a4
NOT_A_ROBOT <76723917+Not-A-Normal-Robot@users.noreply.github.com>
2022-07-12 21:50:23 +0700

    Fix crash when getting TM+6 and above

--- parts/modes/secret_grade.lua
+++ parts/modes/secret_grade.lua
@@ -1,5 +1,26 @@
-local ranks={"Grade 10","Grade 9","Grade 8","Grade 7","Grade 6","Grade 5","Grade 4","Grade 3","Grade 2","Grade 1","S1","S2","S3","S4","S5","S6","S7","S8","S9","GM","GM+","TM","TM+","TM+₂","TM+₃", "TM+₄","TM+₅"}
--- index:         1         2          3        4         5          6         7        8         9         10     11   12   13   14   15   16   17   18   19   20   21    22   23     24     25      26     27
+local function getSmallNum(num)
+    local smalldigit={[0]="₀","₁","₂","₃","₄","₅","₆","₇","₈","₉"}
+    local str=tostring(num)
+    local out=""
+    for i=1,#str do
+        out=out..smalldigit[tonumber(string.sub(str,i,i))]
+    end
+    return out
+end
+
+local function getRank(index)
+    if index<11 then -- rank 10 - 1
+        return "Grade "..tostring(11-index)
+    elseif index<20 then -- S1 - S9 ranks
+        return "S"..index-10
+    elseif index<24 then -- GM, GM+, TM, TM+ ranks
+        local r={"GM","GM+","TM","TM+"}
+        return r[index-19]
+    else
+        return "TM+"..getSmallNum(index-22)
+    end
+end
+
 return{
     env={
         drop=180,lock=180,
@@ -8,7 +29,7 @@ return{
         bg='bg2',bgm='race',
     },
     score=function(P)return{P.modeData.rankPts,P.stat.piece}end,
-    scoreDisp=function(D)return ranks[D[1]].."   "..D[2].." Pieces"end,
+    scoreDisp=function(D)return getRank(D[1]).."   "..D[2].." Pieces"end,
     comp=function(a,b)return a[1]>b[1]or a[1]==b[1]and a[2]<b[2]end,
     getRank=function(P)
         local G=P.modeData.rankPts
