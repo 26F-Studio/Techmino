2491a436c211b937cba5e6ebf9f91694e07459e6
MrZ_26 <1046101471@qq.com>
2022-09-29 08:42:28 +0800

    修复生存L难度显示maxspeed文本时机不正确

--- parts/eventsets/survivor_l.lua
+++ parts/eventsets/survivor_l.lua
@@ -18,7 +18,7 @@ return{
                         table.insert(P.atkBuffer,{line=generateLine(P.holeRND:random(10)),amount=4,countdown=t,cd0=t,time=0,sent=false,lv=3})
                         P.atkBufferSum=P.atkBufferSum+4
                         P.stat.recv=P.stat.recv+4
-                        if D.wave==60 then
+                        if D.wave==90 then
                             P:_showText(text.maxspeed,0,-140,100,'appear',.6)
                         end
                         P:shakeField(3)
