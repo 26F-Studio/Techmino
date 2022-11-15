b41fbe244085459a11da4d431ec80d662463cb6e
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2022-11-15 19:18:38 +0700

    finish up coding, haven't translated

---
+++ parts/modes/classic_beyond.lua
@@ -0,0 +1,23 @@
+return {
+    env={
+        center=0,ghost=0,
+        smooth=false,
+        face={0,0,2,2,2,0,0},
+        eventSet='classic_beyond',
+        bg='rgb',bgm='1980s',
+    },
+    slowMark=true,
+    score=function(P) return {P.stat.score,P.stat.row} end,
+    scoreDisp=function(D) return D[1].."   "..D[2].." Lines" end,
+    comp=function(a,b) return a[1]>b[1] or a[1]==b[1] and a[2]<b[2] end,
+    getRank=function(P)
+        local L=P.stat.row
+        return
+        L>=100 and 5 or
+        L>=80 and 4 or
+        L>=60 and 3 or
+        L>=40 and 2 or
+        L>=20 and 1 or
+        L>=1 and 0
+    end,
+}
