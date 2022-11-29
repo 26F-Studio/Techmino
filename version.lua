8b7c270cf61781319e59581e987b43326ee9e659
MrZ_26 <1046101471@qq.com>
2022-11-30 01:40:26 +0800

    版本推进

--- version.lua
+++ version.lua
@@ -1,7 +1,7 @@
 return {
-    ["apkCode"]=452,
-    ["code"]=1708,
-    ["string"]="V0.17.8",
+    ["apkCode"]=453,
+    ["code"]=1709,
+    ["string"]="V0.17.9",
     ["room"]="ver A-9",
     ["name"]="暂停 Break",
 }
