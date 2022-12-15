9acd8b54e3f2375fe60c3c02db529c6b58110e07
MrZ_26 <1046101471@qq.com>
2022-12-16 00:10:57 +0800

    提前改一下版本号

--- version.lua
+++ version.lua
@@ -1,7 +1,7 @@
 return {
-    ["apkCode"]=453,
-    ["code"]=1709,
-    ["string"]="V0.17.9",
+    ["apkCode"]=454,
+    ["code"]=1710,
+    ["string"]="V0.17.10",
     ["room"]="ver A-9",
     ["name"]="暂停 Break",
 }
