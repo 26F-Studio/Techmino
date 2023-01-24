127a192d5fd545c06f10adae39b328704365e320
MrZ_26 <1046101471@qq.com>
2023-01-25 03:41:49 +0800

    Update dig_checker_10l.lua

--- parts/eventsets/dig_checker_10l.lua
+++ parts/eventsets/dig_checker_10l.lua
@@ -1,16 +1,3 @@
-local garbagePatterns={
-    {0,21,0,21,0,21,0,21,0,21},
-    {21,0,21,0,21,0,21,0,21,0}
-}
-local pushGarbage=function(P)
-    local t=P.showTime*2
-    table.insert(P.field,1,LINE.new(0,true))
-    table.insert(P.visTime,1,LINE.new(t))
-    for i=1,10 do
-        P.field[1][i]=garbagePatterns[P.modeData.parity+1][i]
-    end
-    P.modeData.parity=(P.modeData.parity+1)%2
-end
 return {
     mesDisp=function(P)
         setFont(55)
@@ -22,9 +9,13 @@ return {
         end
     end,
     task=function(P)
-        P.modeData.parity=P.holeRND:random(2)-1
-        for _=1,10 do
-            pushGarbage(P)
+        local r=P.holeRND:random(0,1)
+        for y=1,10 do
+            P.field[y]=LINE.new(0,true)
+            P.visTime[y]=LINE.new(P.showTime*2)
+            for x=1,10 do
+                P.field[y][x]=(x+y+r)%2*21
+            end
         end
         P.fieldBeneath=0
     end,
