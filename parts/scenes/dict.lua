fda99a46750cb0e7f3127a518b8ea0f61ce09a66
Squishy (C6H12O6+NaCl+H2O) <106439598+SweetSea-ButImNotSweet@users.noreply.github.com>
2023-07-26 00:47:43 +0700

    Attempt to rework Zictionary scene

--- parts/scenes/dict.lua
+++ parts/scenes/dict.lua
@@ -1,6 +1,8 @@
 local gc=love.graphics
 
-local inputBox=WIDGET.newInputBox{name='input',x=20,y=110,w=762,h=60,font=40,limit=32}
+local infoBox =WIDGET.newTextBox {name='infoBox',x=320,y=180,w=840,h=526,font=25,fix=true}
+local inputBox=WIDGET.newInputBox{name='input',  x=20, y=110,w=762,h=60, font=40,limit=32}
+
 local int,abs=math.floor,math.abs
 local min,sin=math.min,math.sin
 local ins=table.insert
@@ -17,6 +19,10 @@ local selected-- Selected option
 local scrollPos-- Scroll down length
 
 local lastSearch-- Last searched string
+local lastSelected -- Last selected item
+local justSearched -- Just searched or not?
+
+local currentFontSize=25 -- Current font size, default: 25 
 
 local typeColor={
     help=COLOR.Y,
@@ -29,10 +35,17 @@ local typeColor={
     english=COLOR.B,
     name=COLOR.lV,
 }
+
 local function _filter(word_org)
     local word=word_org
     word=word:gsub("[Tt]etris",CHAR.zChan.thinking)
     if FNNS then word=word:gsub("[Pp]\97\116\114\101\111\110",CHAR.zChan.qualified) end
+    return word,word_org
+end
+local function _defilter(word_org)
+    local word=word_org
+    word=word:gsub(CHAR.zChan.thinking,"Tetris")
+    if FNNS then word=word:gsub(CHAR.zChan.qualified,"P\97\116\114\101\111\110") end
     return word,word_org
 end
 local function _scanDict(D)
@@ -52,6 +65,7 @@ local function _getList() return result[1] and result or dict end
 local function _clearResult()
     TABLE.cut(result)
     selected=1
+    justSearched=true
     scrollPos=0
     searchWait,lastSearch=0,false
     scene.widgetList.copy.hide=false
@@ -59,13 +73,10 @@ end
 local function _search()
     local input=inputBox:getText():lower()
     _clearResult()
-    -- This change was made by Sea. Should the search function prioritize the best match result and move it to the top of the search result
-    -- Usually yes except Vietnamese version of Zictionary
-    local prioritizeBestMatch=not SETTING.locale:find"vi"
     local first
     for i=1,#dict do
         local pos=find(dict[i].title:lower(),input,nil,true) or find(dict[i].keywords:lower(),input,nil,true)
-        if pos==1 and not first and prioritizeBestMatch then
+        if pos==1 and not first then
             ins(result,1,dict[i])
             first=true
         elseif pos then
@@ -74,6 +85,7 @@ local function _search()
     end
     if #result>0 then
         SFX.play('reach')
+        justSearched=true
     end
     lastSearch=input
 end
@@ -87,10 +99,10 @@ function scene.enter()
 
     searchWait=0
     selected=1
-    scrollPos=0
+    scene.widgetList.listBox:select(1)
 
     lastSearch=false
-    WIDGET.focus(inputBox)
+    if not MOBILE then WIDGET.focus(inputBox) end
     BG.set('rainbow')
 end
 
@@ -98,26 +110,16 @@ function scene.wheelMoved(_,y)
     WHEELMOV(y)
 end
 function scene.keyDown(key)
-    if key=='up' then
-        if selected and selected>1 then
-            selected=selected-1
-            if selected<scrollPos+1 then
-                scrollPos=scrollPos-1
-            end
-            scene.widgetList.copy.hide=false
-        end
-    elseif key=='down' then
-        if selected and selected<#_getList() then
-            selected=selected+1
-            if selected>scrollPos+15 then
-                scrollPos=selected-15
-            end
-            scene.widgetList.copy.hide=false
-        end
+    selected=scene.widgetList.listBox.selected
+    local currentX,currentY=love.mouse.getPosition()
+    if key=='up' or key == 'down' then
+        if WIDGET.isFocus(scene.widgetList.listBox)
+        then scene.widgetList.listBox:scroll(key=='up' and -1 or 1)
+        else infoBox:scroll(key == "up" and -5 or 5) end
     elseif key=='left' or key=='pageup' then
-        for _=1,12 do scene.keyDown('up') end
+        scene.widgetList.listBox:arrowKey('up')
     elseif key=='right' or key=='pagedown' then
-        for _=1,12 do scene.keyDown('down') end
+        scene.widgetList.listBox:arrowKey('down')
     elseif key=='application' then
         local url=_getList()[selected].url
         if url then love.system.openURL(url) end
@@ -172,39 +174,44 @@ local function listStencil()
     GC.rectangle('fill',20,180,280,526)
 end
 function scene.draw()
+
     gc.setColor(COLOR.dX)
     gc.rectangle('fill',300,180,958,526,5)
     gc.rectangle('fill',20,180,280,526,5)
     gc.setLineWidth(2)
     gc.setColor(COLOR.Z)
-    gc.rectangle('line',300,180,958,526,5)
+    -- gc.rectangle('line',300,180,958,526,5)
     gc.rectangle('line',20,180,280,526,5)
 
     local list=_getList()
-    gc.setColor(COLOR.Z)
-    local t=list[selected].content
-    setFont(
-        #t>900 and 15 or
-        #t>600 and 20 or
-        #t>400 and 25 or
-        30
-    )
-    gc.printf(t,306,180,950)
-
     setFont(30)
-    gc.setColor(1,1,1,.4+.05*sin(TIME()*12.6))
-    gc.rectangle('fill',20,143+35*(selected-scrollPos),280,35)
 
-    GC.stencil(listStencil)
-    GC.setStencilTest('equal',1)
-    setFont(30)
-    for i=1,min(#list,15) do
-        local y=142+35*i
-        i=i+scrollPos
-        local item=list[i]
-        GC.shadedPrint(item.title,30,y,'left',1,COLOR.D,typeColor[item.type])
+    if lastSelected~=scene.widgetList.listBox.selected or justSearched then
+        -- If searching then update the list
+        if justSearched then scene.widgetList.listBox:setList(_getList()) end
+
+        -- Then update the info box :3
+        lastSelected=scene.widgetList.listBox.selected
+        selected=lastSelected
+        justSearched=false
+        gc.setColor(COLOR.Z)
+        local _w,t=FONT.get(25):getWrap(list[selected].content,820)
+        infoBox:setTexts(t)
+        scene.widgetList.copy.hide=false
     end
-    GC.setStencilTest()
+
+    -- gc.setColor(1,1,1,.4+.05*sin(TIME()*12.6))
+    -- gc.rectangle('fill',20,143+35*(selected-scrollPos),280,35)
+
+    -- GC.stencil(listStencil)
+    -- GC.setStencilTest('equal',1)
+    -- for i=1,min(#list,15) do
+    --     local y=142+35*i
+    --     i=i+scrollPos
+    --     local item=list[i]
+    --     GC.shadedPrint(item.title,30,y,'left',1,COLOR.D,typeColor[item.type])
+    -- end
+    -- GC.setStencilTest()
 
     if searchWait>0 then
         local r=TIME()*2
@@ -215,16 +222,35 @@ function scene.draw()
 end
 
 scene.widgetList={
-    WIDGET.newText{name='book',   x=20,y=15,font=70,align='L',fText=CHAR.icon.zBook},
-    WIDGET.newText{name='title',  x=100,y=15,font=70,align='L'},
+    WIDGET.newText   {name='book',    x=20,  y=15, font=70,align='L',fText=CHAR.icon.zBook},
+    WIDGET.newText   {name='title',   x=100, y=15, font=70,align='L'},
+    -- Draw list box
+    WIDGET.newListBox{name='listBox', x=20,  y=180,w=280,h=526,font=30,lineH=35,drawF=function(item,id,ifSel)
+        -- Background
+        if ifSel then
+            gc.setColor(1,1,1,.4)
+            gc.rectangle('fill',0,0,280,35)
+        end
+
+        -- Name & color
+        -- GC.stencil(listStencil)
+        -- GC.setStencilTest('equal',1)
+        local item=_getList()[id]
+        GC.shadedPrint(item.title,10,-3,'left',1,COLOR.D,typeColor[item.type])
+        -- GC.setStencilTest()
+    end},
+    -- Draw list box /
     inputBox,
-    WIDGET.newKey{name='link',    x=1120,y=655,w=80,font=55,fText=CHAR.icon.globe, code=pressKey'application',hideF=function() return not _getList()[selected].url end},
-    WIDGET.newKey{name='copy',    x=1210,y=655,w=80,font=50,fText=CHAR.icon.copy,  code=pressKey'cC'},
-    WIDGET.newKey{name='up',      x=1120,y=475,w=80,font=50,fText=CHAR.key.up,     code=pressKey'up',hide=not MOBILE},
-    WIDGET.newKey{name='down',    x=1120,y=565,w=80,font=50,fText=CHAR.key.down,   code=pressKey'down',hide=not MOBILE},
-    WIDGET.newKey{name='pageup',  x=1210,y=475,w=80,font=50,fText=CHAR.icon.toUp,  code=pressKey'pageup',hide=not MOBILE},
-    WIDGET.newKey{name='pagedown',x=1210,y=565,w=80,font=50,fText=CHAR.icon.toDown,code=pressKey'pagedown',hide=not MOBILE},
-    WIDGET.newButton{name='back', x=1165,y=60,w=170,h=80,sound='back',font=60,fText=CHAR.icon.back,code=backScene},
+    infoBox,
+    WIDGET.newKey    {name='link',     x=1210,y=575,w=80,font=55,fText=CHAR.icon.globe, code=pressKey'application',hideF=function() return not _getList()[scene.widgetList.listBox.selected].url end},
+    WIDGET.newKey    {name='copy',     x=1210,y=665,w=80,font=50,fText=CHAR.icon.copy,  code=pressKey'cC'},
+
+    WIDGET.newKey    {name='pageup',   x=1210,y=395,w=80,font=50,fText=CHAR.icon.toUp,  code=pressKey'pageup'},
+    WIDGET.newKey    {name='pagedown', x=1210,y=485,w=80,font=50,fText=CHAR.icon.toDown,code=pressKey'pagedown'},
+
+    WIDGET.newKey    {name='zoomsmall',x=1210,y=215,w=80,font=50,fText="a"             ,},
+    WIDGET.newKey    {name='zoombig',  x=1210,y=305,w=80,font=50,fText="A"             ,},
+    WIDGET.newButton {name='back',     x=1165,y=60, w=170,h=80,sound='back',font=60,fText=CHAR.icon.back,code=backScene},
 }
 
 return scene
