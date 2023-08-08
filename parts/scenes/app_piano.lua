8217a063a5ce5f0e848ea2723c7fef90bff15dde
Squishy (C6H12O6+NaCl+H2O) <106439598+SweetSea-ButImNotSweet@users.noreply.github.com>
2023-08-08 20:01:55 +0700

    Add virtual keyboard layout

--- parts/scenes/app_piano.lua
+++ parts/scenes/app_piano.lua
@@ -49,5 +49,60 @@ end
 
 scene.widgetList={
     WIDGET.newButton{name='back', x=1140,y=640,w=170,h=80,sound='back',font=60,fText=CHAR.icon.back,code=backScene},
+
+    -- Number row:  01234567890-=
+    WIDGET.newKey   {name='key1' ,x= 115,y=231,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='key2' ,x= 210,y=231,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='key3' ,x= 305,y=231,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='key4' ,x= 400,y=231,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='key5' ,x= 495,y=231,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='key6' ,x= 590,y=231,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='key7' ,x= 685,y=231,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='key8' ,x= 780,y=231,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='key9' ,x= 875,y=231,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='key0' ,x= 970,y=231,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='key01',x=1065,y=231,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='key02',x=1160,y=231,w=80 ,h=80 ,sound=false ,fText=''},
+
+    -- Top row:     QWERTYUIOP[]\
+    WIDGET.newKey   {name='keyQ' ,x=  65,y=326,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyW' ,x= 160,y=326,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyE' ,x= 255,y=326,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyR' ,x= 350,y=326,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyT' ,x= 445,y=326,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyY' ,x= 540,y=326,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyU' ,x= 635,y=326,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyI' ,x= 730,y=326,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyO' ,x= 825,y=326,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyP' ,x= 920,y=326,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyP1',x=1015,y=326,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyP2',x=1110,y=326,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyP3',x=1205,y=326,w=80 ,h=80 ,sound=false ,fText=''},
+
+    -- Home row     ASDFGHJKL;''<ENTER>
+    WIDGET.newKey   {name='keyA' ,x= 115,y=421,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyS' ,x= 210,y=421,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyD' ,x= 305,y=421,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyF' ,x= 400,y=421,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyG' ,x= 495,y=421,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyH' ,x= 590,y=421,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyJ' ,x= 685,y=421,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyK' ,x= 780,y=421,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyL' ,x= 875,y=421,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyL1',x= 970,y=421,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyL2',x=1065,y=421,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyEn',x=1160,y=421,w=80 ,h=80 ,sound=false ,fText=''},
+
+    -- Bottom row   ZXCVBNM,./
+    WIDGET.newKey   {name='keyZ' ,x= 196,y=516,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyX' ,x= 291,y=516,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyC' ,x= 386,y=516,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyV' ,x= 481,y=516,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyB' ,x= 576,y=516,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyN' ,x= 671,y=516,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyM' ,x= 766,y=516,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyM1',x= 861,y=516,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyM2',x= 956,y=516,w=80 ,h=80 ,sound=false ,fText=''},
+    WIDGET.newKey   {name='keyM3',x=1051,y=516,w=80 ,h=80 ,sound=false ,fText=''},
 }
 return scene
