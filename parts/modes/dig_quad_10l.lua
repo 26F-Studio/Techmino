7719b74b09e6665be13d8d37dcc1e175f8c00eca
Imple Lee <80144331+ImpleLee@users.noreply.github.com>
2024-04-24 03:38:15 +0800

    fix text in dig quad 10l (#1103)
    fix #1102

--- parts/modes/dig_quad_10l.lua
+++ parts/modes/dig_quad_10l.lua
@@ -31,7 +31,7 @@ return {
         end,
     },
     score=function(P) return {P.stat.dig_quad,P.stat.time} end,
-    scoreDisp=function(D) return D[1].." Techrash "..STRING.time(D[2]) end,
+    scoreDisp=function(D) return D[1].." Lines "..STRING.time(D[2]) end,
     comp=function(a,b) return a[1]>b[1] or a[1]==b[1] and a[2]<b[2] end,
     getRank=function(P)
         local dig=P.stat.dig_quad
