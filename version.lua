3f0d4d3cdf94af5c0c0cb8be9731c160d1c2bf28
MrZ_26 <1046101471@qq.com>
2024-05-22 01:19:20 +0800

    更新版本号和更新历史

--- version.lua
+++ version.lua
@@ -1,7 +1,7 @@
 return {
-    ["apkCode"]=1716,
-    ["code"]=1716,
-    ["string"]="V0.17.16",
+    ["apkCode"]=1717,
+    ["code"]=1717,
+    ["string"]="V0.17.17",
     ["room"]="ver A-12",
     ["name"]="暂停 Break",
 }
