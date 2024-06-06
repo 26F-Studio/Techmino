137f9a0d55b6e213eca4ba3c5b7ea645d67c67bd
MrZ_26 <1046101471@qq.com>
2024-06-06 17:38:05 +0800

    修两个消四模式的评级不等号错误

--- parts/modes/techrash_n.lua
+++ parts/modes/techrash_n.lua
@@ -12,7 +12,7 @@ return {
             T>=100 and 5 or
             T>=75 and 4 or
             T>=50 and 3 or
-            T==30 and 2 or
+            T>=30 and 2 or
             T>=15 and 1 or
             T>=5 and 0
     end,
