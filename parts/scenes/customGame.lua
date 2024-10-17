26F-Studio/Techmino

commit 5793b7ca38aa202c8e7f7896d14236ef9116f06b

fix custom games (#1139)
Now you can `play custom_` without going through the `customGame` scene

parts/scenes/customGame.lua
@@ -23,7 +23,10 @@ local function apply_locals()
     TABLE.clear(CUSTOMENV)
     TABLE.cover(CUSTOMGAME_LOCAL.customenv,CUSTOMENV)
 end
-do -- Initialize fields, sequence, missions, gameEnv for cutsom game
+
+local scene={}
+
+function scene.initialize() -- Initialize fields, sequence, missions, gameEnv for cutsom game
     local fieldData=loadFile('conf/customBoards','-string -canSkip')
     local fieldReinit=false
     if not fieldData then
@@ -64,6 +67,7 @@ do -- Initialize fields, sequence, missions, gameEnv for cutsom game
         TABLE.complete(customData,CUSTOMGAME_LOCAL.customenv)
     end
     TABLE.complete(require"parts.customEnv0",CUSTOMGAME_LOCAL.customenv)
+    apply_locals()
 end
 
 local sList={
@@ -86,8 +90,6 @@ local sList={
     holdMode={'hold','swap','skip'},
 }
 local modUsed
-
-local scene={}
 
 function scene.enter()
     destroyPlayers()
@@ -143,7 +145,6 @@ local function _play(mode)
         end
     end
     saveFile(CUSTOMGAME_LOCAL.customenv,'conf/customEnv')
-    apply_locals()
     loadGame('custom_'..mode,true)
 end
 

parts/modes/custom_clear.lua
@@ -1,3 +1,5 @@
+require'parts.scenes.customGame'.initialize()
+
 return {
     env={},
     load=function()

parts/modes/custom_puzzle.lua
@@ -1,6 +1,8 @@
 local gc_setColor,gc_draw=love.graphics.setColor,love.graphics.draw
 local ply_applyField=PLY.draw.applyField
 
+require'parts.scenes.customGame'.initialize()
+
 return {
     env={
         fkey1=function(P) P.modeData.showMark=1-P.modeData.showMark end,

commit 45db3f9c6c58a8f7a1906533040099336c38614c

Fix a wierd bug
Steps to reproduce:
1. reset everything in custom game
2. restart the game
3. go to the custom game scene
4. go to the custom sequence scene and set a new sequence
5. leave the scene and go to the custom game scene
6. begin to clear

Unintended behavior:
The sequence is still the default old sequence (7-bag)

Intended behavior:
The sequence is the newly set sequence

Rationale:
The bug was introduced by #1139.
It deleted a line of `apply_locals()` which I wrongly believed that the custom modes will execute initialize() anyway, but it turned out initialization is only executed during the start of the whole game.
The patch is simple: just add back the deleted line

parts/scenes/customGame.lua
@@ -145,6 +145,7 @@ local function _play(mode)
         end
     end
     saveFile(CUSTOMGAME_LOCAL.customenv,'conf/customEnv')
+    apply_locals()
     loadGame('custom_'..mode,true)
 end
 