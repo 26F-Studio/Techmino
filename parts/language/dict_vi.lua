12ae789239b77995351a24d8bba8ef58e1d63e77
MrZ_26 <1046101471@qq.com>
2024-11-08 23:55:52 +0800

    更新词典关于方块中文维基的内容

--- parts/language/dict_vi.lua
+++ parts/language/dict_vi.lua
@@ -77,7 +77,7 @@ Bạn muốn đóng góp vào bản dịch? Bạn có thể vào trang dự án 
 08. Bot: Cold Clear, ZZZbot
 
 09. Wiki; các trang web bày setup & cung cấp câu đố, chia sẻ setup
-        09A. Wiki: Huiji Wiki, Wiki Hard Drop, tetris.wiki, Tetris Wiki Fandom
+        09A. Wiki: Tetris Wiki CN, Wiki Hard Drop, tetris.wiki, Tetris Wiki Fandom
         09B. Bày setup: Four.lol, Tetris Hall, Tetris Template Collections, tetristemplate.info, 4-Wide Trainer
         09C. Chia sẻ câu đố: TTT, TTPC, NAZO, TPO
         09D. Chia sẻ setup: Fumen, Fumen bản Điện thoại
@@ -1436,12 +1436,12 @@ NHÓM 07: MỘT VÀI CƠ CHẾ VÀ CHẾ ĐỘ CỦA MỘT SỐ GAME
         "",
         ""
     },
-    {"Huiji Wiki",
-        "nhom09a huiji wiki",
-        "help",
-        "(灰机wiki)\n\nMột wiki về Tetris của những người đam mê Tetris từ các nhóm và chi nhánh của Cộng đồng Nghiên cứu Tetris Trung Quốc. Hiện tại hầu hết các trang đều được tham khảo và dịch từ Wiki Hard Drop và Tetris Wiki. Liên kết sẽ dẫn bạn tới bản tiếng Trung giản thể.",
-        "https://tetris.huijiwiki.com",
-    },
+    -- {"Tetris Wiki CN",
+    --     "china tetris wiki",
+    --     "help",
+    --     "A Chinese Tetris wiki by Tetris enthusiasts from Chinese Tetris Research Community groups and affiliates. Most pages have been referenced and translated from Hard Drop Wiki and Tetris Wiki for now. Link in Simplified Chinese.",
+    --     "http://tetriswiki.cn",
+    -- },
     {"Wiki Hard Drop",
         "nhom09a harddrop hd wiki",
         "help",
