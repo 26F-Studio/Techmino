f2e2dd6309f571b24190c42895bffb2ca956ac80
SweetSea <106439598+SweetSea-ButImNotSweet@users.noreply.github.com>
2025-02-11 21:23:36 +0700

    Bug patch for multiplayer chat and log viewer (#1194)
    * Viewlog fix
    
    * Fix log viewer
    
    * Fix on-screen key get pressed with emojis
    
    * Little improvement (I think?)
    
    * Update net_game.lua
    
    ---------
    
    Co-authored-by: MrZ_26 <1046101471@qq.com>

--- parts/scenes/viewlog.lua
+++ parts/scenes/viewlog.lua
@@ -1,34 +1,58 @@
 local fullLog,currentLogID
-local logTimeList={}
+local logTimestampList={}
 local currentLogText={}
+local colorBarWidth=0
 
 local gc=love.graphics
-local min,max=math.min,math.max
+local min=math.min
 local scene={}
 
 local textBox=WIDGET.newTextBox {x= 30,y= 45,w=1000,h=540,font=25,fix=true}
 local logList=WIDGET.newSelector{x=305,y=640,w= 550,name='list',color='O',fText='Crash log',list={''},disp=function() return '' end,code=function() end}
 
-local function updateText()
-    currentLogText=fullLog[currentLogID]:split('\n')
-    do
-        local _,wt=getFont(25):getWrap(fullLog[currentLogID],975)
-        textBox:setTexts(wt)
+local function updateText(noLogFound)
+    if noLogFound then
+        textBox:setTexts{"Techmino is fun!", "It's good to know that there is nothing to read here! :)"}
+    else
+        currentLogText=fullLog[currentLogID]:split('\n')
+        do
+            local _,wt=getFont(25):getWrap(fullLog[currentLogID],975)
+            textBox:setTexts(wt)
+        end
+        logList.select=currentLogID
+        logList.selText=currentLogText[1]
+        colorBarWidth=min((currentLogID-1)/(#fullLog-1),1)*170+1055 -- 170 is the width of full color bar, 1055 is the X of the beginning of the bar
     end
-    logList.select=currentLogID
-    logList.selText=currentLogText[1]
 end
 
 local function noLogFound()
+    currentLogID=nil
     local _w=scene.widgetList
     _w.home.hide=true;_w.list.hide=true
     _w.endd.hide=true;_w.del .hide=true
     _w.copy.hide=true;_w.delA.hide=true
 
+    updateText(true)
     logList.list={''}
-    logList.select=false
     logList.disp=function() return '' end
-    logList.code=function() end
+    logList:reset()
+end
+
+local function logFound()
+    currentLogID=1
+    local _w=scene.widgetList
+    _w.home.hide=false;_w.list.hide=false
+    _w.endd.hide=false;_w.del .hide=false
+    _w.copy.hide=false;_w.delA.hide=false
+
+    logList.list=logTimestampList
+    logList.select=1
+    logList.disp=function() return currentLogText[1] end
+    logList.code=function(_,s)
+        scene.keyDown(s>currentLogID and 'right' or 'left')
+        updateText()
+    end
+    updateText()
     logList:reset()
 end
 
@@ -37,31 +61,13 @@ function scene.enter()
 
     if fullLog=='/conf/error.log not found' then noLogFound()
     else
-        -- Fix data first
+        -- Fix data first (there maybe a duplicate 'Traceback' word)
         fullLog=table.concat(fullLog:split('\n\nTraceback'),'\nTraceback'):split('\n\n')
 
         -- Get timestamps and add into logTimeList for the selector
-        TABLE.reverse(fullLog)
-        for i,d in pairs(fullLog) do logTimeList[i]=d:split('\n')[1] end
-
-        currentLogID=1
-        updateText()
-
-        local _w=scene.widgetList
-        _w.home.hide=false;_w.list.hide=false
-        _w.endd.hide=false;_w.del .hide=false
-        _w.copy.hide=false;_w.delA.hide=false
-
-        logList.list=logTimeList
-        logList.select=false
-        logList.disp=function() return currentLogText[1] end
-        logList.code=function(_,s)
-            if s>currentLogID
-            then scene.keyDown('right')
-            else scene.keyDown('left') end
-            updateText()
-        end
-        logList:reset()
+        TABLE.reverse(fullLog); TABLE.clear(logTimestampList)
+        for i,d in pairs(fullLog) do logTimestampList[i]=d:split('\n')[1] end
+        logFound()
     end
 end
 local deleteOld,deleteAll
@@ -78,18 +84,18 @@ do
             sureTime=-1e99
             scene.widgetList.del.color=COLOR.dY
             do
-                local temp=TABLE.sub(TABLE.copy(fullLog),1,25)
-                fullLog=TABLE.copy(temp)
-                temp=table.concat(temp,'\n\n')..'\n\n'
+                local temp1=TABLE.sub(TABLE.copy(fullLog),1,25)
+                local temp2=table.concat(temp1,'\n\n')..'\n\n'
+                fullLog=TABLE.copy(temp1)
 
-                logTimeList=TABLE.sub(logTimeList,1,25)
+                logTimestampList=TABLE.sub(logTimestampList,1,25)
                 currentLogID=min(logList.select,25)
                 logList.select=currentLogID
-                logList.list=logTimeList
+                logList.list=logTimestampList
                 updateText()
 
                 TASK.removeTask_code(task_redButton)
-                FILE.save(temp,'/conf/error.log','-string')
+                FILE.save(temp2,'/conf/error.log','-string')
             end
         else
             sureTime=TIME()
@@ -115,7 +121,7 @@ do
 
             noLogFound()
             TASK.removeTask_code(task_redButton)
-            SCN.swapTo('viewLog','none')
+            SCN.swapTo('viewlog','none')
         else
             sureTime=TIME()
             MES.new('warn',text.sureDelete)
@@ -156,8 +162,15 @@ function scene.keyDown(key)
 end
 
 function scene.draw()
+    if not currentLogID then return end
+
     setFont(40)
-    gc.print(currentLogID..' / '..#fullLog,1000,800)
+    gc.printf(currentLogID..'\n'..#fullLog,1055,400,170,'right')
+    gc.setColor(1,1,1,0.8)
+    gc.setLineWidth(3)
+    gc.line(1055,450,1225,450)
+    gc.setColor(COLOR.cyan)
+    gc.line(1055,450,colorBarWidth,450)
 end
 
 scene.widgetList={
