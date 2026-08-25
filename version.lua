e21a28d9bc79bc964a4c6ae4faf65ddf82fc02ef
ItzShino <143888260+ItzMeNeon@users.noreply.github.com>
2026-08-25 14:13:52 +0800

    Init commit to rework on the game

--- version.lua
+++ version.lua
@@ -1,7 +1,7 @@
 return {
     ["apkCode"]=1722,
     ["code"]=1722,
-    ["string"]="V0.17.22",
+    ["string"]="v0.3",
     ["room"]="ver A-15",
-    ["name"]="暂停 Break",
+    ["name"]="Beta Non-Upstream",
 }
